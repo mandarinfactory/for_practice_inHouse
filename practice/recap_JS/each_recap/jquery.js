9ad43f4f1c8a3230abb4d23220cf536cb0de4bb2
mandarin <hoojunee@gmail.com>
2022-11-14 01:41:08 +0900

    practice_22.11.14

--- practice/recap_JS/each_recap/jquery.js
+++ practice/recap_JS/each_recap/jquery.js
@@ -2,7 +2,7 @@ $(function () {
     /* ex_one, two --------------------------------------------------------------------------- */
     // ${}가 아닌 parent, siblings, children인 node를 이용해서 function을 만들어본다! ${}로 많이쓰긴한다.
     function searchNumber (numX) {
-        var a = 0; /*  클릭할때마다 점수가 변하면 안되니까 click-event 안에서 변수선언! 
+        var a = 0; /*  클릭할때마다 점수가 변하면 안되니까 click-event 안에서 변수선언 ! 
         each-event안에서 하면 점수가 늘지를 않는다! 0 <=> 1 왔다갔다함. */
         var error =false; // for validation
         numX.parent().siblings('div').each(function () {
@@ -19,14 +19,17 @@ $(function () {
         if(error){
             alert('모든 문제를 체크하고 확인해주시기 바랍니다.')
             return false;
+            /* return -으로 하면 정확히 어떤 값을 return하는건지 알 수 없기때문에, 
+            정확하게 return false라고 명시해주는게 좋다.
+            만약, return으로 해놓으면 null값을 반환한다. */
         }// error값이 true인경우의 if
         numX.parent().siblings('.result').text(a); //한번만 나오면 되니까! each-event밖에서!
-    }//searchNumber
+    }//function - searchNumber
     function eraseNumber (numY) {
         /* $('.ex_one input').attr('checked', false) => 이것도 되지만, 현재는 prop을 권장! */
         numY.parent().siblings('div').children('input').prop('checked', false)
         numY.parent().siblings('.result').text('그 결과는..?')
-    }//earseNumber
+    }//function - earseNumber
     $('.ex_one .button_result, .ex_two .button_result').click(function () {
         searchNumber($(this))
     })//click(.button_result / ex_one)
