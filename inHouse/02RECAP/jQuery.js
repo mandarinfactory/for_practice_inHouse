059720aa53b16658711154e8dc81424588207f41
mandarin <hoojunee@gmail.com>
2022-11-21 17:06:13 +0900

    jqeury_22.11.21

--- inHouse/02RECAP/jQuery.js
+++ inHouse/02RECAP/jQuery.js
@@ -1,8 +1,10 @@
 $(function () {
     function scrollBox () {
         var scrTop = $(window).scrollTop();
+        var winHeight = $(window).innerHeight();
+        var boxHeight = $('.box1').innerHeight();
         var box1Top = $('.box1').offset().top;
-        if(scrTop >= box1Top){
+        if(scrTop >= box1Top - winHeight * 0.25 - boxHeight * 0.5){
             $('.box1').addClass('active')
         } else {
             $('.box1').removeClass('active')
