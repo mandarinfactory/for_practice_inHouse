8416684b8acca03cbc09acbe2b2c4c57e65edb92
mandarin <hoojunee@gmail.com>
2022-11-21 16:17:16 +0900

    jqeury_22.11.21

--- inHouse/02RECAP/jQuery.js
+++ inHouse/02RECAP/jQuery.js
@@ -1,3 +1,18 @@
 $(function () {
+    function scrollBox () {
+        var scrTop = $(window).scrollTop();
+        if(scrTop >= 200){
+            $('.box1').addClass('active')
+        } else {
+            $('.box1').removeClass('active')
+        }//if-else
+    }//function
+
+    scrollBox();
+    $(window).scroll(function () {
+        scrollBox();
+    }).resize(function () {
+        scrollBox();
+    })//scroll && resize
 
 })//document.ready
