378e99f1aa5faff218d4d7fc68e4326c4e5e1a23
mandarin <hoojunee@gmail.com>
2022-11-22 17:37:29 +0900

    jqeury_22.11.22

--- inHouse/04.scroll-RECAP/jQuery.js
+++ inHouse/04.scroll-RECAP/jQuery.js
@@ -7,15 +7,23 @@ $(function () {
         $('nav a').removeClass('active') /* 일단 active-class 다 끄고 시작! */
         if (scrTop < offstTwo - 40) {
             $('.a1').addClass('active')
+            $('section > p').removeClass('active')
+            $('.section1 > p').addClass('active')
         }
         else if(scrTop >= offstTwo - 40 && scrTop < offstThree - 40){// >= ~ 부터 <  ~ 까지
             $('.a2').addClass('active')
+            $('section > p').removeClass('active')
+            $('.section2 > p').addClass('active')
         } 
-        else if(scrTop >= offstThree - 40 && scrTop < offstFour - 40){// >= ~ 부터 <  ~ 까지
+        else if(scrTop >= offstThree - 40 && scrTop <= offstFour - 40){// >= ~ 부터 <  ~ 까지
             $('.a3').addClass('active')
+            $('section > p').removeClass('active')
+            $('.section3 > p').addClass('active')
         } 
         else {// >= ~ 부터 <  ~ 까지
             $('.a4').addClass('active')
+            $('section > p').removeClass('active')
+            $('.section4 > p').addClass('active')
         } 
     }//function
 
@@ -29,5 +37,10 @@ $(function () {
 
     $('nav a').click(function (e) {
         e.preventDefault()
+        var sectionNum = $(this).attr('href');
+        window.scrollTo({
+            top: $(`.section${sectionNum}`).offset().top - 40,
+            behavior : 'smooth'
+        })
     })//click
 })//document.ready
