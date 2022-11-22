4e233af30ac3abb4edc80fc573128fceaade680e
mandarin <hoojunee@gmail.com>
2022-11-22 16:09:01 +0900

    jqeury_22.11.22

--- inHouse/04.scroll-RECAP/jQuery.js
+++ inHouse/04.scroll-RECAP/jQuery.js
@@ -1,3 +1,22 @@
 $(function () {
+    function scrollSpy () {
+        var scrTop = $(window).scrollTop();
+        var offstTwo = $('.section2').offset().top;
+        var offstThree = $('.section3').offset().top;
+
+        if(scrTop + 40 >= offstTwo && scrTop + 40 < offstThree){
+            $('.a2').addClass('active')
+        } else {
+            $('.a2').removeClass('active')
+        }
+    }//function
+
+    scrollSpy();
+
+    $(window).resize(function () {
+        scrollSpy();
+    }).scroll(function () {
+        scrollSpy();
+    })//resize && scroll
 
 })//document.ready
