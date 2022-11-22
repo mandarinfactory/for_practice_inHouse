354ba92e35bfe2658335fa87cd2b4513a2eb7526
mandarin <hoojunee@gmail.com>
2022-11-22 16:32:21 +0900

    jqeury_22.11.22

--- inHouse/04.scroll-RECAP/jQuery.js
+++ inHouse/04.scroll-RECAP/jQuery.js
@@ -3,12 +3,20 @@ $(function () {
         var scrTop = $(window).scrollTop();
         var offstTwo = $('.section2').offset().top;
         var offstThree = $('.section3').offset().top;
-
-        if(scrTop + 40 >= offstTwo && scrTop + 40 < offstThree){
-            $('.a2').addClass('active')
-        } else {
-            $('.a2').removeClass('active')
+        var offstFour = $('.section4').offset().top;
+        $('nav a').removeClass('active') /* 일단 active-class 다 끄고 시작! */
+        if (scrTop < offstTwo - 40) {
+            $('.a1').addClass('active')
         }
+        else if(scrTop >= offstTwo - 40 && scrTop < offstThree - 40){// >= ~ 부터 <  ~ 까지
+            $('.a2').addClass('active')
+        } 
+        else if(scrTop >= offstThree - 40 && scrTop < offstFour - 40){// >= ~ 부터 <  ~ 까지
+            $('.a3').addClass('active')
+        } 
+        else if(scrTop >= offstFour - 40){// >= ~ 부터 <  ~ 까지
+            $('.a4').addClass('active')
+        } 
     }//function
 
     scrollSpy();
