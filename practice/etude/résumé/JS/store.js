0a2ea30f4a920c75c59fc30e63611990e19152e4
mandarin <hoojunee@gmail.com>
2022-12-23 00:00:16 +0900

    résumé_22.12.22

---
+++ practice/etude/résumé/JS/store.js
@@ -0,0 +1,14 @@
+window.viewBoxArr = [
+    {
+        path : './IMG/template_01.jpg'
+    },
+    {
+        path : './IMG/template_02.jpg'
+    },
+    {
+        path : './IMG/template_03.jpg'
+    },
+    {
+        path : './IMG/template_04.jpg'
+    },
+]
