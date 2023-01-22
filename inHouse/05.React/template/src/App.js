1436d1a38b0946284978ff875e3191dcf07c3ffb
mandarin <hoojunee@gmail.com>
2023-01-22 15:34:47 +0900

    react_23.01.22
    react 강의 다시 시작함

--- inHouse/05.React/template/src/App.js
+++ inHouse/05.React/template/src/App.js
@@ -3,11 +3,20 @@
 import "./App.css";
 
 function App() {
+
+  const post = '강남 우동 맛집';
   
   return (
     <div className="App">
-      
+      <div className="black_nav">
+        <h3 style={{color : 'firebrick'}}>블로그!</h3>
+      </div>
+      <div className="list">
+        <h4>글제목</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <h4>{post}</h4>
     </div>
-  )
+    )
   }
 export default App;
