e74f7ba267c31910dee83b9d131657595dbf32e7
mandarin <hoojunee@gmail.com>
2023-01-27 14:49:11 +0900

    all pull_23.01.27

---
+++ inHouse/05.React/website/src/App.js
@@ -0,0 +1,12 @@
+import { Button } from 'react-bootstrap';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Button variant="primary">Primary</Button>{' '}
+    </div>
+  );
+}
+
+export default App;
