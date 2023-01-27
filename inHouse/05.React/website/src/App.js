38debfe47144847755b27b16c53923ec4517bca2
mandarin <hoojunee@gmail.com>
2023-01-27 18:08:28 +0900

    reactShop_23.01.27
    components들  bootstrap으로 만들어봄

--- inHouse/05.React/website/src/App.js
+++ inHouse/05.React/website/src/App.js
@@ -1,11 +1,41 @@
-import { Button } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Button variant="primary">Primary</Button>{' '}
-    </div>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home" className='title'>ReactShop</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Others</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div className='main-bg'></div>
+        <div className="container">
+          <div className="row">
+            <div className="col-md-4">
+              <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="80%"/>
+              <h4>상품명</h4>
+              <p>상품설명</p>
+            </div>
+            <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="80%"/>
+              <h4>상품명</h4>
+              <p>상품설명</p>
+            </div>
+            <div className="col-md-4">
+            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="80%"/>
+              <h4>상품명</h4>
+              <p>상품설명</p>
+            </div>
+          </div>
+        </div> 
+      </div>
   );
 }
 
