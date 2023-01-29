7be31bc037f775085597aee7837613e72b9e2671
mandarin <hoojunee@gmail.com>
2023-01-29 21:17:04 +0900

    reactShop_23.01.27
    main, detail 페이지 나누고 detail은 Comp로 따로 만듬 + CSS

--- inHouse/05.React/website/src/App.js
+++ inHouse/05.React/website/src/App.js
@@ -1,35 +1,50 @@
 /* eslint-disable */
 
 import data from './js/data'
+import CompSection from './components/CompSection';
 import { useState } from 'react';
 import { Container, Nav, Navbar } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import CompSection from './components/CompSection';
+import { Routes, Route, Link } from 'react-router-dom'
+import CompDetail from './components/CompDetail';
 
 function App() {
 
   const [shoes] = useState(data)
     return (
     <div className="App">
+
+
+
       <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home" className='title'>ReactShop</Navbar.Brand>
           <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Others</Nav.Link>
+            <Link to='/' className='button'>홈</Link>
+            <Link to='/detail' className='button'>상세페이지</Link>
           </Nav>
         </Container>
       </Navbar>
-      <div className='main-bg'></div>
-        <div className="container">
-          <div className="row">
-            <CompSection shoes={shoes}/>
-          </div>
-        </div> 
+
+      <Routes>
+        <Route path='/' element={
+            <>
+            <div className='main-bg'></div>
+              <div className="container">
+                <div className="row">
+                  <CompSection shoes={shoes}></CompSection>
+                </div>
+              </div> 
+        </>
+        } />
+        <Route path='/detail' element={
+          <CompDetail></CompDetail>
+        } />
+      </Routes>
       </div>
   );
 }
+
 
 export default App;
--- inHouse/05.React/website/src/components/CompSection.jsx
+++ inHouse/05.React/website/src/components/CompSection.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable */
+
 import React from 'react';
 
 const CompSection = props => {
