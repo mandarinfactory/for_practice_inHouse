b5d403f72855e873956b858f27010069cf217164
mandarin <hoojunee@gmail.com>
2023-02-05 23:26:24 +0900

    react_spotify_23.02.02
    전반적인 section들은 clone coding 했으나 오류 수정이 부분부분 필요해보임

--- inHouse/05.React/lyriks/src/pages/AroundYou.jsx
+++ inHouse/05.React/lyriks/src/pages/AroundYou.jsx
@@ -1,5 +1,23 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
 
-const CountryTracks = () => <div>CountryTracks</div>;
+import { Error, Loader, SongCard } from '../components'; 
 
-export default CountryTracks;
+const AroundYou = () => {
+    const [country, setCountry] = useState('');
+    const [loading, setLoading] = useState(true);
+    const { activeSong, isPlaying } = useSelector(state => state.player);
+
+    useEffect(() => {
+        axios.get(`https://geo.ipify.org/api/v2/country?apiKey=at_69TmvypfY4VExf6sD6UivKyPMSWUB&ipAddress=8.8.8.8`)
+        //at_69TmvypfY4VExf6sD6UivKyPMSWUB
+    }, [country]);
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default AroundYou;
--- inHouse/05.React/lyriks/src/redux/services/shazamCore.js
+++ inHouse/05.React/lyriks/src/redux/services/shazamCore.js
@@ -10,10 +10,16 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
             },
         }),
         endpoints : builder => ({
-            getTopCharts : builder.query({ query : () => '/charts/world' })
+            getTopCharts : builder.query({ query : () => '/charts/world' }),
+            getSongDetails : builder.query({ query : ({ songid }) => `/tracks/details?track_id=${songid}`}),
+            getSongRelated : builder.query({ query : ({ songid }) => `/tracks/related?track_id=${songid}`}),
+            getArtistDetails : builder.query({ query : (artistId) => `/artists/details?artist_id=${artistId}`}),
         }),
     });
 
     export const {
         useGetTopChartsQuery,
+        useGetSongDetailsQuery,
+        useGetSongRelatedQuery,
+        useGetArtistDetailsQuery,
     } = shazamCoreApi;
