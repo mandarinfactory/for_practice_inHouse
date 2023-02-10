a5a46d8c34b412ea8a675c42fe3a65ebda734695
mandarin <hoojunee@gmail.com>
2023-02-10 12:30:15 +0900

    react_website_22.02.10
    갈아엎고 다시 시작함

--- inHouse/05.React/website/src/App.js
+++ inHouse/05.React/website/src/App.js
@@ -1,18 +1,47 @@
-import './App.css';
-import Button from 'react-bootstrap/Button';
+import "./App.css";
+import { Container, Nav, Navbar } from "react-bootstrap";
 
 function App() {
   return (
     <div className="App">
-      <Button variant="primary">Primary</Button>
-      <Button variant="secondary">Secondary</Button>
-      <Button variant="success">Success</Button>
-      <Button variant="warning">Warning</Button>
-      <Button variant="danger">Danger</Button>
-      <Button variant="info">Info</Button>
-      <Button variant="light">Light</Button>
-      <Button variant="dark">Dark</Button>
-      <Button variant="link">Link</Button>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home" className="title">
+            ReactShop
+          </Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home" className="subtitle">
+              Home
+            </Nav.Link>
+            <Nav.Link href="#features" className="subtitle">
+              Item
+            </Nav.Link>
+            <Nav.Link href="#pricing" className="subtitle">
+              Cart
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+      <div className="main-bg" />
+      <div className="container">
+        <div className="row">
+          <div className="col-md-4">
+            <img src='https://codingapple1.github.io/shop/shoes1.jpg' width='80%'></img>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+          <div className="col-md-4">
+            <img src='https://codingapple1.github.io/shop/shoes2.jpg' width='80%'></img>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+          <div className="col-md-4">
+            <img src='https://codingapple1.github.io/shop/shoes3.jpg' width='80%'></img>
+            <h4>상품명</h4>
+            <p>상품설명</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
