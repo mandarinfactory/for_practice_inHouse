9947de7bf333e2a523be29ef07e107d19d731d40
mandarin <hoojunee@gmail.com>
2023-02-13 11:42:10 +0900

    react_website_22.02.13
    router-dom navigator, outlet

--- inHouse/05.React/website/src/App.js
+++ inHouse/05.React/website/src/App.js
@@ -1,6 +1,6 @@
 import { useState } from "react";
 import { Container, Nav, Navbar } from "react-bootstrap";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, Link, useNavigate, Outlet } from "react-router-dom";
 import "./App.css";
 import CompDetail from "./asset/components/CompDetail";
 import CompShoes from "./asset/components/CompShoes";
@@ -8,6 +8,7 @@ import data from "./asset/js/data";
 
 function App() {
   const [shoes] = useState(data);
+  const navigate = useNavigate();
 
   return (
     <div className="App">
@@ -43,12 +44,40 @@ function App() {
             </>
           }
         />
-        <Route path="/detail" element={
-          <CompDetail />
-        } />
+        <Route path="/detail" element={<CompDetail />} />
+        <Route path="/about" element={<About />}>
+          <Route path="member" element={<div>멤버임</div>} />
+          <Route path="location" element={<div>위치정보임</div>} />
+        </Route>
+        <Route path="/event" element={<Event />}>
+          <Route
+            path="one"
+            element={<div>첫 주문시 케일 + 양배추즙 서비스 증정~!</div>}
+          />
+          <Route path="two" element={<div>생일기념 COUPON 받기~!</div>} />
+        </Route>
+        <Route path="*" element={<div>없는페이지입니다~!</div>}></Route>
       </Routes>
     </div>
   );
 }
+
+const About = () => {
+  return (
+    <>
+      <h4>회사정보</h4>
+      <Outlet></Outlet>
+    </>
+  );
+};
+
+const Event = () => {
+  return (
+    <>
+      <h4>오늘의 EVENT~!</h4>
+      <Outlet></Outlet>
+    </>
+  );
+};
 
 export default App;
