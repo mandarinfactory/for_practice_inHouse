b048d8625b7594ccb51d5864ae650cef9dfbf350
mandarin <hoojunee@gmail.com>
2023-02-23 21:39:58 +0900

    JS_22.02.23
    정리 및 TypeScript 기초시작함

---
+++ inHouse/03.TypeScript/index.js
@@ -0,0 +1,6 @@
+var User = /** @class */ (function () {
+    function User(name) {
+        this.name = name;
+    }
+    return User;
+}());
---
+++ inHouse/03.TypeScript/index.ts
@@ -0,0 +1,6 @@
+class User {
+    name : string;
+    constructor (name : string) {
+        this.name = name;
+    }
+}
