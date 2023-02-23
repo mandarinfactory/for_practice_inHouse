5d1068ff049b1744d0b90f0d36082202decb4bdc
mandarin <hoojunee@gmail.com>
2023-02-23 16:52:51 +0900

    JS_22.02.23
    JS기초 정리

--- inHouse/02.ECMAscript/기초/project/apps.js
+++ inHouse/02.ECMAscript/기초/project/apps.js
@@ -1,3 +1,11 @@
+document.querySelector('.btn-primary').addEventListener('click', () => {
+    const inputIdVal = document.querySelector('.id').value
+    const inputPwVal = document.querySelector('.pw').value
+    if(inputIdVal == '' || inputPwVal == '') {
+        alert('빈칸을 확인하고 다시 제출해주십시오.')
+    }//if
+})
+
 document.querySelector('.navbar-toggler').addEventListener('click', () => {
     document.querySelector('.list-group').classList.toggle('show');
 })
@@ -6,7 +14,11 @@ document.querySelector('#login').addEventListener('click', () => {
     document.querySelector('.black-bg').style.display =' block'
     document.querySelector('.black-bg').classList.add('show-modal')
 })
+
 document.querySelector('#close').addEventListener('click', () => {
     document.querySelector('.black-bg').style.display =' none'
     document.querySelector('.black-bg').classList.remove('show-modal')
 })
+
+/* 전송버튼 누르면 ---
+저기 input에 입력한 값이 공백이면 --- 알림창 띄어주기 ---- */
