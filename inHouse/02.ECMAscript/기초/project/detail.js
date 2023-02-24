849556100d1ec374dc907db63d237b94032a9783
mandarin <hoojunee@gmail.com>
2023-02-24 15:20:01 +0900

    JS_22.02.24
    Javascript기초다지기! 리뷰!

---
+++ inHouse/02.ECMAscript/기초/project/detail.js
@@ -0,0 +1,24 @@
+/* 
+버튼0 누르면
+버튼??에 붙은 orange 클래스명 제거
+버튼 0에 orange 클래스명 추가
+모든 div에 붙은 show 클래스명 제거
+div0에 show 클래스명 추가
+*/
+document.querySelectorAll('.tab-button').forEach(v => {
+    v.addEventListener('click', e => {
+        let tarVal = e.target.value 
+        document.querySelectorAll(".tab-button")[`${tarVal}`].addEventListener("click", (e) => {
+          document.querySelectorAll(".tab-button").forEach(v => {
+            v.classList.remove("orange");
+          })//forEach
+          e.target.classList.add("orange");
+          document.querySelectorAll('.tab-content').forEach(v => {
+              v.classList.remove("show");
+            })//forEach
+            document.querySelectorAll(".tab-content")[`${tarVal}`].classList.add("show")
+        })//click
+    })//click
+})//forEach
+
+
