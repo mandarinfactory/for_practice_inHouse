19e0bc37e90c1d6e0c93fb1ec24ec4a7af350113
mandarin <hoojunee@gmail.com>
2023-03-03 22:36:57 +0900

    JS_22.03.03
    portpolio_3D modeling

--- inHouse/04.stuff/project_02/src/components/Navbar.jsx
+++ inHouse/04.stuff/project_02/src/components/Navbar.jsx
@@ -21,6 +21,22 @@ const Navbar = () => {
           <img src={logo} alt="logo" className='w-9 h-9 object-contain'/>
           <p className='text-white text-[18px] font-bold cursor-pointer'>Mandarin <span className='sm:block hidden'>| portpolio</span></p>
         </Link>
+        <ul className='list-none hidden sm:flex flex-row gap-10'>
+          {
+            navLinks.map((link) => (
+              <li
+                key={link.id}
+                className={`${
+                  active === link.title ?
+                  'text-white' :
+                  'text-secondary'
+                } hover:text-white text-[18px] font-medium cursor-pointer`}
+              >
+                <a href={`#${link.id}`}>{link.title}</a>
+              </li>
+            ))
+          }
+        </ul>
       </div>
     </nav>
   )
