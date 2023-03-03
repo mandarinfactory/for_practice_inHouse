df831aa348516856c385e49048ecfcf96a5251e6
mandarin <hoojunee@gmail.com>
2023-03-03 22:41:30 +0900

    JS_22.03.03
    portpolio_3D modeling

--- inHouse/04.stuff/project_02/src/components/Navbar.jsx
+++ inHouse/04.stuff/project_02/src/components/Navbar.jsx
@@ -1,45 +1,58 @@
-import React, { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 
-import { styles } from '../styles';
-import { navLinks } from '../constants';
-import { logo, menu, close } from '../assets';
+import { styles } from "../styles";
+import { navLinks } from "../constants";
+import { logo, menu, close } from "../assets";
 
 const Navbar = () => {
-  const [active, setActive] = useState('');
+  const [active, setActive] = useState("");
+  const [toggle, setToggle] = useState(false);
   return (
-    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+    <nav
+      className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}
+    >
       <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
         <Link
-        to='/'
-        className='flex items-center gap-2'
-        onClick={() => {
-          setActive('')
-          window.scrollTo(0,0)
-        }}
+          to="/"
+          className="flex items-center gap-2"
+          onClick={() => {
+            setActive("");
+            window.scrollTo(0, 0);
+          }}
         >
-          <img src={logo} alt="logo" className='w-9 h-9 object-contain'/>
-          <p className='text-white text-[18px] font-bold cursor-pointer'>Mandarin <span className='sm:block hidden'>| portpolio</span></p>
+          <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
+          <p className="text-white text-[18px] font-bold cursor-pointer">
+            Mandarin <span className="sm:block hidden">| portpolio</span>
+          </p>
         </Link>
-        <ul className='list-none hidden sm:flex flex-row gap-10'>
-          {
-            navLinks.map((link) => (
-              <li
-                key={link.id}
-                className={`${
-                  active === link.title ?
-                  'text-white' :
-                  'text-secondary'
-                } hover:text-white text-[18px] font-medium cursor-pointer`}
-              >
-                <a href={`#${link.id}`}>{link.title}</a>
-              </li>
-            ))
-          }
+        <ul className="list-none hidden sm:flex flex-row gap-10">
+          {navLinks.map((link) => (
+            <li
+              key={link.id}
+              className={`${
+                active === link.title ? "text-white" : "text-secondary"
+              } hover:text-white text-[18px] font-medium cursor-pointer`}
+              onClick={() => setActive(link.title)}
+            >
+              <a href={`#${link.id}`}>{link.title}</a>
+            </li>
+          ))}
         </ul>
+        <div className="sm:hidden flex flex-1 justify-end itmes-center">
+          <img
+            src={toggle ? close : menu}
+            alt="menu"
+            className="w-[28px] h-[28px] object-contain cursor-pointer"
+            onClick={() => setToggle(!toggle)}
+          />
+          <div>
+            
+          </div>
+        </div>
       </div>
     </nav>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
