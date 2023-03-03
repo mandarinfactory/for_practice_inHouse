9f303f6700fb761b2ef4bbd21f8c2bd34ce42a96
mandarin <hoojunee@gmail.com>
2023-03-04 00:29:59 +0900

    JS_22.03.03
    portpolio_3D modeling

--- inHouse/04.stuff/project_02/src/components/canvas/Computers.jsx
+++ inHouse/04.stuff/project_02/src/components/canvas/Computers.jsx
@@ -1,13 +1,15 @@
 import { Suspense, useEffect, useState } from 'react';
-/* import { Canvas } from '@react-three/fiber';
-import { OrbitControls, Preload, useGLTF } from '@react-three/drei'; */
+import { Canvas } from '@react-three/fiber';
+import { OrbitControls, Preload, useGLTF } from '@react-three/drei';
 
 import CanvasLoader from '../Loader';
 
 const Computers = () => {
-  const computer = useGLTF('./desktop_pc')
+  const computer = useGLTF('./desktop_pc/scene.gltf')
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={0.15}/>
+    </mesh>
   )
 }
 
