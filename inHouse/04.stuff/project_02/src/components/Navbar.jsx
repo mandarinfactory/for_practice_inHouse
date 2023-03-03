0c16f151b4af53322f41fdd46d0c6231e36fe845
mandarin <hoojunee@gmail.com>
2023-03-03 22:25:30 +0900

    JS_22.03.03
    portpolio_3D modeling

--- inHouse/04.stuff/project_02/src/components/Navbar.jsx
+++ inHouse/04.stuff/project_02/src/components/Navbar.jsx
@@ -6,8 +6,23 @@ import { navLinks } from '../constants';
 import { logo, menu, close } from '../assets';
 
 const Navbar = () => {
+  const [active, setActive] = useState('');
   return (
-    <div>Navbar</div>
+    <nav className={`${styles.paddingX} w-full flex items-center py-5 fixed top-0 z-20 bg-primary`}>
+      <div className="w-full flex justify-between items-center max-w-7xl mx-auto">
+        <Link
+        to='/'
+        className='flex items-center gap-2'
+        onClick={() => {
+          setActive('')
+          window.scrollTo(0,0)
+        }}
+        >
+          <img src={logo} alt="logo" className='w-9 h-9 object-contain'/>
+          <p className='text-white text-[18px] font-bold cursor-pointer'>Mandarin <span className='sm:block hidden'>| portpolio</span></p>
+        </Link>
+      </div>
+    </nav>
   )
 }
 
