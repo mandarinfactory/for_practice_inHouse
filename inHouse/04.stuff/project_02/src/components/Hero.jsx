78086ed3b2cf4fdfd9a926dac92106692b66fd6a
mandarin <hoojunee@gmail.com>
2023-03-04 00:21:05 +0900

    JS_22.03.03
    portpolio_3D modeling

--- inHouse/04.stuff/project_02/src/components/Hero.jsx
+++ inHouse/04.stuff/project_02/src/components/Hero.jsx
@@ -11,7 +11,13 @@ const Hero = () => {
       >
         <div className="flex flex-col justify-center items-center mt-5">
           <div className="w-5 h-5 rounded-full bg-[#915eff]"/>
+          <div className="w-1 sm:h-80 h-40 violet-gradient"/>
         </div>
+        <div>
+          <h1 className={`${styles.heroHeadText}text-white`}>안녕하세요. <span className="text-[#915eff]">이호준</span>입니다.</h1>
+          <p className={`${styles.heroSubText} mt-5 text-white-100`}><span className="text-[#915eff]">일관성있는</span> 배움의 자세를 통해 성장하는 개발자입니다.</p>
+        </div>
+        <ComputersCanvas />
       </div>
     </section>
   );
