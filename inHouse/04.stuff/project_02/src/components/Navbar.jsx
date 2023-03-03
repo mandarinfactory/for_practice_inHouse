a201d03e35acfa92ae5dede93c7f85bbd0d72f61
mandarin <hoojunee@gmail.com>
2023-03-03 22:52:34 +0900

    JS_22.03.03
    portpolio_3D modeling

--- inHouse/04.stuff/project_02/src/components/Navbar.jsx
+++ inHouse/04.stuff/project_02/src/components/Navbar.jsx
@@ -22,8 +22,8 @@ const Navbar = () => {
           }}
         >
           <img src={logo} alt="logo" className="w-9 h-9 object-contain" />
-          <p className="text-white text-[18px] font-bold cursor-pointer">
-            Mandarin <span className="sm:block hidden">| portpolio</span>
+          <p className="text-white text-[18px] font-bold cursor-pointer flex">
+            Mandarin&nbsp;<span className="sm:block hidden">| portpolio</span>
           </p>
         </Link>
         <ul className="list-none hidden sm:flex flex-row gap-10">
@@ -46,8 +46,27 @@ const Navbar = () => {
             className="w-[28px] h-[28px] object-contain cursor-pointer"
             onClick={() => setToggle(!toggle)}
           />
-          <div>
-            
+          <div
+            className={`${
+              !toggle ? "hidden" : "flex"
+            } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
+          >
+            <ul className="list-none flex justify-end items-start flex-col gap-4">
+              {navLinks.map((link) => (
+                <li
+                  key={link.id}
+                  className={`${
+                    active === link.title ? "text-white" : "text-secondary"
+                  } font-poppins font-medium cursor-pointer text-[16px]`}
+                  onClick={() => {
+                    setToggle(!toggle);
+                    setActive(link.title);
+                  }}
+                >
+                  <a href={`#${link.id}`}>{link.title}</a>
+                </li>
+              ))}
+            </ul>
           </div>
         </div>
       </div>
