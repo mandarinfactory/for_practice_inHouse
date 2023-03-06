e1c89b26baee20bb75efb3cb5fd85dcad7d90f2d
mandarin <hoojunee@gmail.com>
2023-03-07 00:48:53 +0900

    22.03.07
    중급 JavaScript : destructuring

--- inHouse/02.ECMAscript/초중급/project/index03.js
+++ inHouse/02.ECMAscript/초중급/project/index03.js
@@ -35,3 +35,21 @@ function 함수01({ name, age }) {
     console.log(name, age);
 }
 함수01(obj03); // Brian --> name / 42 --> age
+
+// 12.import && export //
+/* 
+1. import 가져올거 from '경로' --> html, React면 Component파일
+2. export default 내보낼거 --> 실제 JS파일
+3. 내보낼거와 가져올거에 이름은 같을 필요가 없다.
+4. export default는 파일당 1회만 사용가능하다. --> 여러개 하려면 export만 써서 내보내야한다. + {}사용!
+5. export는 변수 선언 이후 사용하거나 아님 변수 선언하면서 같이 사용해도 된다.
+6. export로만 내보냈으면 import로 받을때에도 같은이름으로 {}를 사용해서 받아야한다.
+7. export와 export default는 동시에 사용할수 있다.
+8. 단, 두개를 동시에 출력시에는 export default로 내보낸 변수가 가장 최우선으로 앞으로 와야한다.
+*/
+export var aa = 10;
+var bb = 20;
+var cc = 30;
+
+export {bb};
+export default cc;
