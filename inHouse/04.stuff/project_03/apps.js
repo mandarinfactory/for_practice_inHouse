190543ccf0dd7e757a5d2c304357d264713c70d9
mandarin <hoojunee@gmail.com>
2023-03-10 01:09:16 +0900

    22.03.10
    another project_기본적인 틀만 짰음 : 큰 골격, naver API 연동

--- inHouse/04.stuff/project_03/apps.js
+++ inHouse/04.stuff/project_03/apps.js
@@ -1,6 +1,26 @@
-var mapOptions = {
-    center: new naver.maps.LatLng(37.3595704, 127.105399),
-    zoom: 10
-};
+let winW = window.innerWidth;
+let winH = window.innerHeight;
 
-var map = new naver.maps.Map('map', mapOptions);
+let mapOptions = {
+  zoom: 10,
+  center: new naver.maps.LatLng(37.4876, 126.7528),
+  size: new naver.maps.Size(winW, winH),
+};
+let map = new naver.maps.Map("map", mapOptions);
+
+let markerOptions = {
+  position: map.getCenter(),
+  map: map,
+};
+let marker = new naver.maps.Marker(markerOptions);
+
+let mapElement = map.getElement();
+let listener = naver.maps.Event.addDOMListener(
+  mapElement,
+  "click",
+  function () {
+    map.setZoom(15);
+    map.setCenter(marker.getPosition());
+    naver.maps.Event.removeDOMListener(listener);
+  }
+);
