24fbb681a97aef61c8c94f07e7e9afcbd1a58a59
mandarin <hoojunee@gmail.com>
2023-03-11 14:30:34 +0900

    22.03.11
    another project_기본적인 틀만 짰음 : 큰 골격, naver API 연동

--- inHouse/04.stuff/project_04/src/App.js
+++ inHouse/04.stuff/project_04/src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {HashRouter, Route} from 'react-router-dom';
+import Home from "./routes/Home";
+import Search from "./routes/Search"
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home}></Route>
+      <Route path="/search" exact={true} component={Search}></Route>
+    </HashRouter>
+    );
 }
 
 export default App;
