05094a74c570d2640cb96297cef52ea3a88bc6a7
mandarin <hoojunee@gmail.com>
2023-03-17 01:15:43 +0900

    22.03.17
    React : useState

--- inHouse/01.React/project/src/App.js
+++ inHouse/01.React/project/src/App.js
@@ -7,6 +7,7 @@ function App() {
 
   let post = '강남 우동 맛집';
   let [ 글제목, set글제목 ] = useState(['남자 코트 추천', '맛집 추천','가을 옷 추천']);
+  let [ 따봉, set따봉 ] = useState(0);
 
   return (
     <div>
@@ -14,8 +15,13 @@ function App() {
         <h4 style={{ color : 'red' }}>BLOG!!</h4>
       </div>
       <div className='list list1'>
-        <h4>{ 글제목[0] }</h4>
+        <h4>{ 글제목[0] } <span onClick={ () => {set따봉(따봉 + 1)} }>👍</span> {따봉} </h4>
         <p>2월 17일 발행</p>
+        <button onClick={() => {
+          let copy = [...글제목]
+          copy[0] = '여자 코드 추천'
+          set글제목(copy);
+        }}>글제목변경</button>
       </div>
       <div className='list list2'>
         <h4>{ 글제목[1] }</h4>
