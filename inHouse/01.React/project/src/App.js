ae4caca6717468bb6636e58216d5a06e9a0043c6
mandarin <hoojunee@gmail.com>
2023-03-17 00:40:51 +0900

    22.03.17
    React다시 기초연습

--- inHouse/01.React/project/src/App.js
+++ inHouse/01.React/project/src/App.js
@@ -1,11 +1,21 @@
 import '../src/App.css';
+import { useState } from 'react';
 
 function App() {
+
+  let post = '강남 우동 맛집';
+  let [ 글제목, set글제목 ] = useState('남자 코트 추천');
+
   return (
     <div>
       <div className='black-nav'>
-        <h4>BLOG!!</h4>
+        <h4 style={{ color : 'red' }}>BLOG!!</h4>
       </div>
+      <div className='list'>
+        <h4>{ 글제목 }</h4>
+        <p>2월 17일 발행</p>
+      </div>
+      <h3>{ post }</h3>
     </div>
   );
 }
