b48df28f81eb318f9141a5e45c16b6b022d6e472
mandarin <hoojunee@gmail.com>
2023-03-18 18:57:52 +0900

    22.03.18
    server기초강의 : NodeJS, MongoDB --> get요청

--- inHouse/05.nodeJS/server.js
+++ inHouse/05.nodeJS/server.js
@@ -12,3 +12,7 @@ app.get('/pet', function(req, res) {
 app.get('/beauty', function(req, res) {
     res.send('방굽! 여기는 BEAUTY 용품을 쇼핑할 수 있는 페이지입니다.')
 })
+//app.get(GET요청-->읽기요청)을 하면 주소창에 URL을 입력해서 server에 GET요청을 할 수 있다.
+app.get('/', function(req, res) {
+    res.sendFile(__dirname + '/index.html');
+})
