5d480c8259bba4de9fc819e7e64e5edaf30ad5fe
mandarin <hoojunee@gmail.com>
2023-03-19 23:16:36 +0900

    22.03.19
    server기초강의 : html파일들 ejs로 통합

--- inHouse/05.nodeJS/server.js
+++ inHouse/05.nodeJS/server.js
@@ -41,18 +41,24 @@ MongoClient.connect(
   }
 );
 
-app.get("/pet", (req, res) => {
+/* app.get("/pet", (req, res) => {
   res.send("방굽! 여기는 PET 용품을 쇼핑할 수 있는 페이지입니다.");
 });
 app.get("/beauty", (req, res) => {
   res.send("방굽! 여기는 BEAUTY 용품을 쇼핑할 수 있는 페이지입니다.");
-});
+}); */
 //app.get(GET요청-->읽기요청)을 하면 주소창에 URL을 입력해서 server에 GET요청을 할 수 있다.
-app.get("/", (req, res) => {
+/* app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
+}); */
+app.get('/', (req, res) => {
+    res.render('index.ejs')
 });
-app.get("/write", (req, res) => {
+/* app.get("/write", (req, res) => {
   res.sendFile(__dirname + "/write.html");
+}); */
+app.get('/write', (req, res) => {
+    res.render('write.ejs')
 });
 
 app.get("/list", (req, res) => {
