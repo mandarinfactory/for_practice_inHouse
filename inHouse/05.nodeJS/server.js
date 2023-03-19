b9dcf0717f0daae65e992168160105d89d91e05b
mandarin <hoojunee@gmail.com>
2023-03-19 10:06:07 +0900

    22.03.19
    server기초강의 : EJS 사용해서 JS사용 및 server에서 data 가져오기

--- inHouse/05.nodeJS/server.js
+++ inHouse/05.nodeJS/server.js
@@ -16,10 +16,8 @@ MongoClient.connect('mongodb+srv://mandarinfactory:tiger6475!@mandarinfactory.ld
     });
     app.listen(8080, () => {
         console.log('listening on 8080');
-    });
+    }); // server를 띄우기 위한 기본 셋팅!(설치한 express 라이브러리 사용방법)
 });
-
-// server를 띄우기 위한 기본 셋팅!(설치한 express 라이브러리 사용방법)
 
 app.get('/pet',(req, res) => {
     res.send('방굽! 여기는 PET 용품을 쇼핑할 수 있는 페이지입니다.')
