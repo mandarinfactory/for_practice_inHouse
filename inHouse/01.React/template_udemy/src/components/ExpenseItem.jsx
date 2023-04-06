9b1442dc0c051e52a3d0cd83658b21e0b766baa0
Ho Jun Lee <hoojunee@gmail.com>
2023-04-07 00:13:24 +0900

    23.04.07
    react 기본적인 기능 배우기(components, props), JS함수(getFullYear, toLocaleString)

--- inHouse/01.React/template_udemy/src/components/ExpenseItem.jsx
+++ inHouse/01.React/template_udemy/src/components/ExpenseItem.jsx
@@ -1,9 +1,16 @@
 import "./ExpenseItem.css";
 
 function ExpenseItem(props) {
+  const year = props.date.getFullYear();
+  const month = props.date.toLocaleString("ko-KR", { month: "long" });
+  const day = props.date.toLocaleString("ko-KR", { day: "2-digit" });
   return (
     <div className="expense-item">
-      <div>{props.date.toISOString()}</div>
+      <div>
+        <div>{year}</div>
+        <div>{month}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}원</div>
