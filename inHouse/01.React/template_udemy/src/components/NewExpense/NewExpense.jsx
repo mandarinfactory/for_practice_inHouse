7f0c2566d3873ecf9a612f0760d25087c4d5c060
Ho Jun Lee <hoojunee@gmail.com>
2023-04-09 00:15:29 +0900

    23.04.08
    add another components

---
+++ inHouse/01.React/template_udemy/src/components/NewExpense/NewExpense.jsx
@@ -0,0 +1,11 @@
+import react from 'react';
+import '../../css/NewExpense.css'
+
+const NewExpense = () => {
+    return 
+    <div className='new-expense'>
+        <form></form>
+    </div>
+};
+
+export default NewExpense;
