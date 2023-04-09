e9f7fb0c5ea40ebf17c07aca446685d07e03453d
Ho Jun Lee <hoojunee@gmail.com>
2023-04-09 14:04:02 +0900

    23.04.09
    부모component <--> 자식component 간의 값 교류하는법

---
+++ inHouse/01.React/template_udemy/src/components/Expense/ExpenseFilter.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "../../css/ExpenseFilter.css";
+
+const ExpensesFilter = (props) => {
+  return (
+    <div className="expenses-filter">
+      <div className="expenses-filter__control">
+        <label>년단위로 확인</label>
+        <select onClick={props.dateHandler}>
+          <option value="2022">2022</option>
+          <option value="2021">2021</option>
+          <option value="2020">2020</option>
+          <option value="2019">2019</option>
+        </select>
+      </div>
+    </div>
+  );
+};
+
+export default ExpensesFilter;
--- inHouse/01.React/template_udemy/src/components/Expense/Expenses.jsx
+++ inHouse/01.React/template_udemy/src/components/Expense/Expenses.jsx
@@ -1,32 +1,39 @@
 import ExpenseItem from "./ExpenseItem";
 import Card from "../Design/Card";
-import '../../css/Expenses.css'
+import "../../css/Expenses.css";
+import ExpensesFilter from "./ExpenseFilter";
 
 function Expenses(props) {
+  const dateHandler = (event) => {
+    console.log(event.target.value);
+  };
   return (
-    <Card className="expenses">
-      <ExpenseItem
-        title={props.expenses[0].title}
-        amount={props.expenses[0].amount}
-        date={props.expenses[0].date}
-      />
-      <ExpenseItem
-        title={props.expenses[1].title}
-        amount={props.expenses[1].amount}
-        date={props.expenses[1].date}
-      />
-      <ExpenseItem
-        title={props.expenses[2].title}
-        amount={props.expenses[2].amount}
-        date={props.expenses[2].date}
-      />
-      <ExpenseItem
-        title={props.expenses[3].title}
-        amount={props.expenses[3].amount}
-        date={props.expenses[3].date}
-      />
-    </Card>
+    <>
+      <ExpensesFilter dateHandler={dateHandler} />
+      <Card className="expenses">
+        <ExpenseItem
+          title={props.expenses[0].title}
+          amount={props.expenses[0].amount}
+          date={props.expenses[0].date}
+        />
+        <ExpenseItem
+          title={props.expenses[1].title}
+          amount={props.expenses[1].amount}
+          date={props.expenses[1].date}
+        />
+        <ExpenseItem
+          title={props.expenses[2].title}
+          amount={props.expenses[2].amount}
+          date={props.expenses[2].date}
+        />
+        <ExpenseItem
+          title={props.expenses[3].title}
+          amount={props.expenses[3].amount}
+          date={props.expenses[3].date}
+        />
+      </Card>
+    </>
   );
-};
+}
 
 export default Expenses;
