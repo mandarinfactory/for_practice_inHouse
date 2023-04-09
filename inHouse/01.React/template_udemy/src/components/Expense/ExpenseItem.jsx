debb41741ff02edb57c29391bbd4cd1d464cb546
Ho Jun Lee <hoojunee@gmail.com>
2023-04-09 21:34:03 +0900

    23.04.09
    부모component <--> 자식component 간의 값 교류하는법, useState()

--- inHouse/01.React/template_udemy/src/components/Expense/ExpenseItem.jsx
+++ inHouse/01.React/template_udemy/src/components/Expense/ExpenseItem.jsx
@@ -1,21 +1,15 @@
-import { useState } from "react";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../Design/Card";
 import "../../css/ExpenseItem.css";
 
 const ExpenseItem = (props) => {
-  const [title, setTitle] = useState(props.title);
-  const clickHandler = () => {
-    setTitle("UPDATED!!!");
-  };
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}원</div>
       </div>
-      <button onClick={clickHandler}>제목 바꾸기</button>
     </Card>
   );
 };
