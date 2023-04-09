78285c2b1f170a38c19cbc2321b527616871f8e9
Ho Jun Lee <hoojunee@gmail.com>
2023-04-09 11:03:17 +0900

    23.04.08
    useState배우면서 복습도 함

--- inHouse/01.React/template_udemy/src/components/NewExpense/ExpenseForm.jsx
+++ inHouse/01.React/template_udemy/src/components/NewExpense/ExpenseForm.jsx
@@ -1,21 +1,34 @@
-import react from "react";
+/* eslint-disable */
+import react, { useState } from "react";
 import "../../css/ExpenseForm.css";
 
 const ExpenseForm = () => {
+  const [ enteredTitle, setEnteredTitle ] = useState('');
+  const [ enteredAmount, setEnteredAmount ] = useState('');
+  const [ enteredDate, setEnteredDate ] = useState('');
+  const titleChHandler = event => {
+    setEnteredTitle(event.target.value);
+  };
+  const amountChHandler = event => {
+    setEnteredAmount(event.target.value);
+  };
+  const dateChHandler = event => {
+    setEnteredDate(event.target.value);
+  };
   return (
     <form>
       <div className="new-expense__controls">
         <div className="new-expense__control">
           <label>제목</label>
-          <input type="text"/>
+          <input type="text" onChange={titleChHandler} />
         </div>
         <div className="new-expense__control">
           <label>총량</label>
-          <input type="number" min="0.01" step="0.01"/>
+          <input type="number" min="0.01" step="0.01" onChange={amountChHandler}/>
         </div>
         <div className="new-expense__control">
           <label>날짜</label>
-          <input type="date" min="2019-01-01" max="2023-12-31"/>
+          <input type="date" min="2019-01-01" max="2023-12-31" onChange={dateChHandler}/>
         </div>
       </div>
       <div className="new-expense__actions">
