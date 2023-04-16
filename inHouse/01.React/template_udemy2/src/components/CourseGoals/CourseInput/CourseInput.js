fa376f1e31f7847fa7383caef0a28e8606e6acbb
Ho Jun Lee <hoojunee@gmail.com>
2023-04-16 21:13:03 +0900

    23.04.16
    styled-components 로 css 구성

--- inHouse/01.React/template_udemy2/src/components/CourseGoals/CourseInput/CourseInput.js
+++ inHouse/01.React/template_udemy2/src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,7 +1,40 @@
 import React, { useState } from "react";
+import styled from 'styled-components';
 
 import Button from "../../UI/Button/Button";
 import "./CourseInput.css";
+
+const formControl = styled.div`
+  margin: 0.5rem 0;
+  & label {
+    font-weight: bold;
+    display: block;
+    margin-bottom: 0.5rem;
+  }
+
+  & input {
+    display: block;
+    width: 100%;
+    border: 1px solid #ccc;
+    font: inherit;
+    line-height: 1.5rem;
+    padding: 0 0.25rem;
+  }
+
+  & input:focus {
+    outline: none;
+    background: #fad0ec;
+    border-color: #8b005d;
+  }
+
+  &.invalid input {
+    border-color: red;
+    background-color: rgb(250, 172, 172);
+  }
+  &.invalid label {
+    color: red;
+  }
+`;
 
 const CourseInput = (props) => {
   const [enteredValue, setEnteredValue] = useState("");
