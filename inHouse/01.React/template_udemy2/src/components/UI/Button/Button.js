6ef10072bd6515c7feb585bcdd33f1e4bf54b3db
Ho Jun Lee <hoojunee@gmail.com>
2023-04-16 20:51:51 +0900

    23.04.16
    styled-components 설치함

--- inHouse/01.React/template_udemy2/src/components/UI/Button/Button.js
+++ inHouse/01.React/template_udemy2/src/components/UI/Button/Button.js
@@ -1,13 +1,15 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import './Button.css';
 
-const Button = props => {
+const Button = styled.button``;
+/* const Button = props => {
   return (
     <button type={props.type} className="button" onClick={props.onClick}>
       {props.children}
     </button>
   );
-};
+}; */
 
 export default Button;
