20e4a13b97c20f627214d122c46e0abed63206a7
Ho Jun Lee <hoojunee@gmail.com>
2023-04-19 00:33:58 +0900

    23.04.19
    버튼 누르면 새로운 list들 뜨게 만들기 (따로 component생성)

--- inHouse/01.React/template_udemy2/src/App.js
+++ inHouse/01.React/template_udemy2/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import AppUser from "./components/Users/AddUser";
 import Card from "./components/UI/Card";
+import UsersList from "./components/Users/UsersList";
 
 function App() {
   return (
     <div>
-      <AppUser />
+      <AppUser/>
+      <UsersList users={[]}/>
     </div>
   );
 }
--- inHouse/01.React/template_udemy2/src/components/Users/AddUser.jsx
+++ inHouse/01.React/template_udemy2/src/components/Users/AddUser.jsx
@@ -1,20 +1,45 @@
-import React from 'react';
-import Card from '../UI/Card';
-import Button from '../UI/Button';
-import classes from '../../css/AddUser.module.css';
+import React, { useState } from "react";
+import Card from "../UI/Card";
+import Button from "../UI/Button";
+import classes from "../../css/AddUser.module.css";
 
 const AppUser = (props) => {
+  const [enteredUsername, setEnteredUsername] = useState("");
+  const [enteredAge, setEnteredAge] = useState("");
   const addUserHandler = (e) => {
     e.preventDefault();
+    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) return;
+    if (+enteredAge < 1) return;
+    console.log(enteredUsername, enteredAge);
+    setEnteredUsername("");
+    setEnteredAge("");
+  };
+
+  const usernameChHandler = (e) => {
+    setEnteredUsername(e.target.value);
+  };
+
+  const ageChHandler = (e) => {
+    setEnteredAge(e.target.value);
   };
 
   return (
     <Card className={classes.input}>
       <form onSubmit={addUserHandler}>
         <label htmlFor="username">유저이름</label>
-        <input id="username" type="text" />
+        <input
+          id="username"
+          type="text"
+          value={enteredUsername}
+          onChange={usernameChHandler}
+        />
         <label htmlFor="age">나이</label>
-        <input id="age" type="number" />
+        <input
+          id="age"
+          type="number"
+          value={enteredAge}
+          onChange={ageChHandler}
+        />
         <Button type="submit">유저추가하기</Button>
       </form>
     </Card>
