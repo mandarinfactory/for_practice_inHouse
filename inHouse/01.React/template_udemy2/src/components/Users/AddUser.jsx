bc3d3ff61fab5e60b3a9cea30bdc774cd0ae420d
Ho Jun Lee <hoojunee@gmail.com>
2023-04-20 00:57:38 +0900

    23.04.20
    state가 아닌 useRef를 이용해서 state 변환주기

--- inHouse/01.React/template_udemy2/src/components/Users/AddUser.jsx
+++ inHouse/01.React/template_udemy2/src/components/Users/AddUser.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useRef } from "react";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import classes from "../../css/AddUser.module.css";
@@ -6,37 +6,30 @@ import ErrorModal from "../UI/ErrorModal";
 import Wrapper from "../Helpers/Wrapper";
 
 const AppUser = (props) => {
-  const [enteredUsername, setEnteredUsername] = useState("");
-  const [enteredAge, setEnteredAge] = useState("");
+  const nameInputRef = useRef();
+  const ageInputRef = useRef();
+
   const [error, setError] = useState();
 
   const addUserHandler = (e) => {
     e.preventDefault();
-    if (enteredUsername.trim().length === 0 || enteredAge.trim().length === 0) {
+    const userName = nameInputRef.current.value;
+    const userAge = ageInputRef.current.value;
+    if (userName.trim().length === 0 || userAge.trim().length === 0) {
       setError({
         title: "유효하지 않은 이름",
         message: "유효한 이름과 나이를 기재해 주세요!(빈칸없이)",
       });
       return;
     }
-    if (+enteredAge < 1) {
+    if (+userAge < 1) {
       setError({
         title: "유효하지 않은 나이",
         message: "유효한 나이를 기재해 주세요!(0보다 큰수로)",
       });
       return;
     }
-    props.onAddUser(enteredUsername, enteredAge);
-    setEnteredUsername("");
-    setEnteredAge("");
-  };
-
-  const usernameChHandler = (e) => {
-    setEnteredUsername(e.target.value);
-  };
-
-  const ageChHandler = (e) => {
-    setEnteredAge(e.target.value);
+    props.onAddUser(userName, userAge);
   };
 
   const errorHandler = () => {
@@ -58,15 +51,13 @@ const AppUser = (props) => {
           <input
             id="username"
             type="text"
-            value={enteredUsername}
-            onChange={usernameChHandler}
+            ref={nameInputRef}
           />
           <label htmlFor="age">나이</label>
           <input
             id="age"
             type="number"
-            value={enteredAge}
-            onChange={ageChHandler}
+            ref={ageInputRef}
           />
           <Button type="submit">유저추가하기</Button>
         </form>
