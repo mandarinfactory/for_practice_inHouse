e00b5634ae6f5b6483d79427ab8e8b11c15b11b7
Ho Jun Lee <hoojunee@gmail.com>
2023-04-23 20:35:00 +0900

    23.04.23
    useEffect공부

--- inHouse/01.React/template_udemy/src/App.js
+++ inHouse/01.React/template_udemy/src/App.js
@@ -8,25 +8,25 @@ const INITIAL_EXPENSES = [
   {
     id: "e1",
     title: "화장지",
-    amount: "5,000",
+    amount: 5000,
     date: new Date(2020, 7, 14),
   },
   {
     id: "e2",
     title: "삼성TV",
-    amount: "1,230,000",
+    amount: 1230000,
     date: new Date(2021, 2, 12),
   },
   {
     id: "e3",
     title: "차보험료",
-    amount: "563,000",
+    amount: 563000,
     date: new Date(2021, 2, 28),
   },
   {
     id: "e4",
     title: "식기세척기",
-    amount: "890,000",
+    amount: 890000,
     date: new Date(2021, 5, 12),
   },
 ];
