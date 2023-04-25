bbd03e5794fcf71213777dd516103abb464de1ee
Ho Jun Lee <hoojunee@gmail.com>
2023-04-26 00:57:22 +0900

    23.04.26
    portpolio_3d modeling
    상단 nav 제작

--- inHouse/04.stuff/template_portfolio/src/App.jsx
+++ inHouse/04.stuff/template_portfolio/src/App.jsx
@@ -1,9 +1,36 @@
+import { BrowserRouter } from "react-router-dom";
+import {
+  About,
+  Contact,
+  Experience,
+  Feedbacks,
+  Hero,
+  Navbar,
+  Tech,
+  Works,
+  StarsCanvas,
+} from "./components";
+
 const App = () => {
   return (
-    <div>
-      일관성있는 개발자 이호준의 포트폴리오입니다.
-    </div>
-  )
-}
+    <BrowserRouter>
+      <div className="relative z-0 bg-primary">
+        <div className="bg-hero-pattern bg-cover bg-no-repeat bg-center">
+          <Navbar/>
+          <Hero/>
+        </div>
+        <About/>
+        <Experience/>
+        <Tech/>
+        <Works/>
+        <Feedbacks/>
+        <div className="relative z-0">
+          <Contact/>
+          <StarsCanvas/>
+        </div>
+      </div>
+    </BrowserRouter>
+  );
+};
 
-export default App
+export default App;
