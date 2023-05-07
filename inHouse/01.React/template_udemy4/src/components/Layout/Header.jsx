fad02a2866fcac59bdff1e11da20a6857a25837c
Ho Jun Lee <hoojunee@gmail.com>
2023-05-07 20:44:47 +0900

    23.05.07
    새로운 react project_음식주문앱

--- inHouse/01.React/template_udemy4/src/components/Layout/Header.jsx
+++ inHouse/01.React/template_udemy4/src/components/Layout/Header.jsx
@@ -1,9 +1,16 @@
 import React from "react";
-import "./Header.module.css";
+import classes from "./Header.module.css";
+import mealsImage from "../../assets/meals.jpg";
 
 const Header = (props) => {
   return <React.Fragment>
-    
+    <header className={classes.header}>
+        <h1>주문도우미</h1>
+        <button>장바구니</button>
+    </header>
+    <div className={classes["main-image"]}>
+        <img src={mealsImage} alt="음식들로 꽉찬 식탁사진"/>
+    </div>
   </React.Fragment>;
 };
 
