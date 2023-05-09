1a5d26796549ac433582f75f3128c53e99248391
Ho Jun Lee <hoojunee@gmail.com>
2023-05-10 00:19:45 +0900

    23.05.10
    새로운 react project_음식주문앱

--- inHouse/01.React/template_udemy4/src/components/Layout/Header.jsx
+++ inHouse/01.React/template_udemy4/src/components/Layout/Header.jsx
@@ -1,12 +1,13 @@
 import React from "react";
 import classes from "./Header.module.css";
 import mealsImage from "../../assets/meals.jpg";
+import HeaderCartButton from "./HeaderCartButton";
 
 const Header = (props) => {
   return <React.Fragment>
     <header className={classes.header}>
         <h1>주문도우미</h1>
-        <button>장바구니</button>
+        <HeaderCartButton/>
     </header>
     <div className={classes["main-image"]}>
         <img src={mealsImage} alt="음식들로 꽉찬 식탁사진"/>
--- inHouse/01.React/template_udemy4/src/components/Layout/HeaderCartButton.jsx
+++ inHouse/01.React/template_udemy4/src/components/Layout/HeaderCartButton.jsx
@@ -1,10 +1,16 @@
 import React from "react";
+import CartIcon from "../Cart/CartIcon";
+import classes from "./HeaderCartButton.module.css"
 
 const HeaderCartButton = (props) => {
-    return <button>
-        <span></span>
-        <span></span>
-        <span></span>
+    return <button className={classes.button}>
+        <span className={classes.icon}>
+            <CartIcon/>
+        </span>
+        <span>당신의 장바구니</span>
+        <span className={classes.badge}>
+            3
+        </span>
     </button>
 };
 
