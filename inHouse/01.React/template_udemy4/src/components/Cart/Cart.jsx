e451d5f819704f6af2a37d6a63ca36b45aa30528
Ho Jun Lee <hoojunee@gmail.com>
2023-05-11 00:54:22 +0900

    23.05.11
    새로운 react project_음식주문앱

--- inHouse/01.React/template_udemy4/src/components/Cart/Cart.jsx
+++ inHouse/01.React/template_udemy4/src/components/Cart/Cart.jsx
@@ -1,7 +1,33 @@
 import classes from "./Cart.module.css";
 
 const Cart = (props) => {
-    return
+  const cartItems = (
+    <ul className={classes["cart-items"]}>
+      {[
+        {
+          id: "c1",
+          name: "초밥",
+          amount: "4",
+          price: 74000,
+        },
+      ].map((item) => (
+        <li>{item.name}</li>
+      ))}
+    </ul>
+  );
+  return (
+    <div>
+      {cartItems}
+      <div className={classes.total}>
+        <span>총수량</span>
+        <span>215800</span>
+      </div>
+      <div className={classes.actions}>
+        <button className={classes["button-alt"]}>닫기</button>
+        <button className={classes.button}>주문</button>
+      </div>
+    </div>
+  );
 };
 
 export default Cart;
