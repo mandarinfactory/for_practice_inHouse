9b98f987998c72b8032654278db08723b19a1861
Ho Jun Lee <hoojunee@gmail.com>
2023-05-10 22:43:52 +0900

    23.05.10
    새로운 react project_음식주문앱

--- inHouse/01.React/template_udemy4/src/components/Meals/MealItem/MealItemForm.jsx
+++ inHouse/01.React/template_udemy4/src/components/Meals/MealItem/MealItemForm.jsx
@@ -3,7 +3,14 @@ import classes from "./MealItemForm.module.css"
 
 const MealItemForm = (props) => {
  return <form className={classes.form}>
-    <input />
+    <Input label="수량" input={{
+      id : "amount",
+      type : "number",
+      min : "1",
+      max : "5",
+      step : "1",
+      defaultValue : "1",
+    }}/>
     <button>추가</button>
  </form>
 };
