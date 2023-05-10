9770242ec8dc6b7775f4f8e5d5489513eb5816b2
Ho Jun Lee <hoojunee@gmail.com>
2023-05-11 01:01:54 +0900

    23.05.11
    새로운 react project_음식주문앱_Modal component 만듦

--- inHouse/01.React/template_udemy4/src/components/UI/Modal.jsx
+++ inHouse/01.React/template_udemy4/src/components/UI/Modal.jsx
@@ -1,7 +1,25 @@
+import React from "react";
 import classes from "./Modal.module.css";
 
+const Backdrop = (props) => {
+  return <div className={classes.backdrop}></div>;
+};
+
+const ModalOverlay = (props) => {
+  return (
+    <div className={classes.modal}>
+      <div className={classes.content}>{props.children}</div>
+    </div>
+  );
+};
+
 const Modal = (props) => {
-    <div className={classes.modal}></div>
+  return (
+    <React.Fragment>
+      <Backdrop />
+      <ModalOverlay>{props.children}</ModalOverlay>
+    </React.Fragment>
+  );
 };
 
 export default Modal;
