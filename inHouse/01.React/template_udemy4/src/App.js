f80817c390f2705c8600dc68fa2313a26894b4f8
Ho Jun Lee <hoojunee@gmail.com>
2023-05-11 21:11:38 +0900

    23.05.11
    새로운 react project_음식주문앱_Modal component 만듦

--- inHouse/01.React/template_udemy4/src/App.js
+++ inHouse/01.React/template_udemy4/src/App.js
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./components/Layout/Header";
 import Meals from "./components/Meals/Meals";
 import Cart from "./components/Cart/Cart";
 
 function App() {
+  const [cartIsShown, setCartIsShown] = useState(false);
   return (
     <React.Fragment>
       <Cart />
