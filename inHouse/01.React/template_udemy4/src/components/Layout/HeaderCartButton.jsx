acbc788ba3f3465725a12ba4b4227da2d25c9cf4
Ho Jun Lee <hoojunee@gmail.com>
2023-05-11 23:57:01 +0900

    23.05.11
    Cart context 추가함--> error 있어서 debugging해야함!

--- inHouse/01.React/template_udemy4/src/components/Layout/HeaderCartButton.jsx
+++ inHouse/01.React/template_udemy4/src/components/Layout/HeaderCartButton.jsx
@@ -1,15 +1,23 @@
-import React from "react";
+import { useContext } from "react";
+
 import CartIcon from "../Cart/CartIcon";
+import CartContext from "../../store/cart-context";
 import classes from "./HeaderCartButton.module.css";
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItem = cartCtx.items.reduce((currentNum, item) => {
+    return currentNum + item.amount;
+  }, 0);
+
   return (
     <button className={classes.button} onClick={props.onClick}>
       <span className={classes.icon}>
         <CartIcon />
       </span>
       <span>당신의 장바구니</span>
-      <span className={classes.badge}>3</span>
+      <span className={classes.badge}>{numberOfCartItem}</span>
     </button>
   );
 };
