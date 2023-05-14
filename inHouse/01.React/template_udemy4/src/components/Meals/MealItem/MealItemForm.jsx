e1ff391ea0a78e0cf587de8216abe09fd47bc5e7
Ho Jun Lee <hoojunee@gmail.com>
2023-05-14 22:18:52 +0900

    23.05.14
    Cart에 useReducer를 사용해서 반응성이 있게 만들어줌

--- inHouse/01.React/template_udemy4/src/components/Meals/MealItem/MealItemForm.jsx
+++ inHouse/01.React/template_udemy4/src/components/Meals/MealItem/MealItemForm.jsx
@@ -1,18 +1,27 @@
 import Input from "../../UI/Input";
-import classes from "./MealItemForm.module.css"
+import classes from "./MealItemForm.module.css";
 
 const MealItemForm = (props) => {
- return <form className={classes.form}>
-    <Input label="수량" input={{
-      id : "amount_" + props.id,
-      type : "number",
-      min : "1",
-      max : "5",
-      step : "1",
-      defaultValue : "1",
-    }}/>
-    <button>추가</button>
- </form>
+  const submitHandler = (event) => {
+    event.preventDefault();
+  };
+
+  return (
+    <form className={classes.form} onSubmit={submitHandler}>
+      <Input
+        label="수량"
+        input={{
+          id: "amount_" + props.id,
+          type: "number",
+          min: "1",
+          max: "5",
+          step: "1",
+          defaultValue: "1",
+        }}
+      />
+      <button>추가</button>
+    </form>
+  );
 };
 
 export default MealItemForm;
