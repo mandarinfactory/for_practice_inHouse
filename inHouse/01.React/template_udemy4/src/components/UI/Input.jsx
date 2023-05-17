639402f5c573a9dfa9ebef0416c3390d9da6468b
Ho Jun Lee <hoojunee@gmail.com>
2023-05-17 23:39:01 +0900

    23.05.17
    Cart에 useReducer를 사용해서 반응성이 있게 만들어줌

--- inHouse/01.React/template_udemy4/src/components/UI/Input.jsx
+++ inHouse/01.React/template_udemy4/src/components/UI/Input.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 import classes from "./Input.module.css";
 
-const Input = React.forwardRef((props) => {
+const Input = React.forwardRef((props, ref) => {
   return (
     <div className={classes.input}>
       <label htmlFor={props.input.id}>{props.label}</label>
-      <input {...props.input} />
+      <input ref={ref} {...props.input} />
     </div>
   );
 });
