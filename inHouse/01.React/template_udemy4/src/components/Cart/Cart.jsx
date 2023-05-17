2de0fc17b8df1d07cbc4fe3f516ba1493ec74383
Ho Jun Lee <hoojunee@gmail.com>
2023-05-18 00:38:14 +0900

    23.05.18
    실제 Cart에 추가한 메뉴들이 나오게 하기

--- inHouse/01.React/template_udemy4/src/components/Cart/Cart.jsx
+++ inHouse/01.React/template_udemy4/src/components/Cart/Cart.jsx
@@ -1,17 +1,18 @@
+import { useContext } from "react";
+
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css";
+import CartContext from "../../store/cart-context";
 
 const Cart = (props) => {
+  const CartCtx = useContext(CartContext);
+
+  const totalAmount = `${CartCtx.totalAmount.toFixed(0)}원`;
+  const hasItems = CartCtx.items.length > 0;
+
   const cartItems = (
     <ul className={classes["cart-items"]}>
-      {[
-        {
-          id: "c1",
-          name: "초밥",
-          amount: "4",
-          price: 74000,
-        },
-      ].map((item) => (
+      {CartCtx.items.map((item) => (
         <li>{item.name}</li>
       ))}
     </ul>
@@ -21,11 +22,13 @@ const Cart = (props) => {
       {cartItems}
       <div className={classes.total}>
         <span>총 수량</span>
-        <span>74000 원</span>
+        <span>{totalAmount}</span>
       </div>
       <div className={classes.actions}>
-        <button className={classes["button-alt"]} onClick={props.onClose}>닫기</button>
-        <button className={classes.button}>주문</button>
+        <button className={classes["button-alt"]} onClick={props.onClose}>
+          닫기
+        </button>
+        {hasItems && <button className={classes.button}>주문</button>}
       </div>
     </Modal>
   );
