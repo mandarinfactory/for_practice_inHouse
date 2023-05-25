46db30324ab9ef8307090f4e52db709715e2c4f4
Ho Jun Lee <hoojunee@gmail.com>
2023-05-25 21:41:14 +0900

    23.05.24
    react 심화이론

--- inHouse/01.React/template_udemy5/src/App.js
+++ inHouse/01.React/template_udemy5/src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from "react";
+import Button from "./components/UI/Button/Button"
 
-import './App.css';
+import "./App.css";
 
 function App() {
+  const [showParagraph, setShowParagraph] = useState(false);
+  const toggleBtnHandler = () => {
+    setShowParagraph(true);
+  };
   return (
     <div className="app">
       <h1>Hi there!</h1>
+      {showParagraph &&  <p>This is New!</p>}
+      <Button onClick={toggleBtnHandler}>Show Paragraph!</Button>
     </div>
   );
 }
