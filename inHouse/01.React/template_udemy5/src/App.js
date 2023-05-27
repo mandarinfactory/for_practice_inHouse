34efcdc51e97ff9e62cd7b162d3220bc78e7a3e8
Ho Jun Lee <hoojunee@gmail.com>
2023-05-27 23:45:54 +0900

    23.05.27
    react 심화이론
    useCallback(react.memo랑 같은 기능을 하는 hook)

--- inHouse/01.React/template_udemy5/src/App.js
+++ inHouse/01.React/template_udemy5/src/App.js
@@ -1,13 +1,15 @@
-import React, { useState } from "react";
+import React, { useState, useCallback } from "react";
+/* useCallback --> component 실행 전반에 걸쳐 함수를 저장할 수 있게 하는 hook이다. */
 import Button from "./components/UI/Button/Button";
 import DemoOutput from "./components/UI/Button/demo/DemoOutput";
 import "./App.css";
 
 function App() {
   const [showParagraph, setShowParagraph] = useState(false);
-  const toggleBtnHandler = () => {
+  const toggleBtnHandler = useCallback(() => {
     setShowParagraph((prevShowParagraph) => !prevShowParagraph);
-  };
+  }, []);
+
   return (
     <div className="app">
       <h1>Hi there!</h1>
@@ -18,3 +20,4 @@ function App() {
 }
 
 export default App;
+ 
