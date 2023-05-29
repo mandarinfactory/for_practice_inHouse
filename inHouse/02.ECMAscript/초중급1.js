ac7051e7f968b3315e0b7442d74186ebc833992a
Ho Jun Lee <hoojunee@gmail.com>
2023-05-30 00:22:49 +0900

    23.05.29
    JS 초중급 복습하기__01. this

--- inHouse/02.ECMAscript/초중급1.js
+++ inHouse/02.ECMAscript/초중급1.js
@@ -1,4 +1,5 @@
 /* 01. this */
+
 console.log(this); // {window}
 
 function 함수 () {
@@ -35,3 +36,34 @@ var 객체3 = {
 };
 
 객체3.data.함수();
+
+function 기계 () {
+    this.이름 = "Kim"; // 객체생성기계(constructor)에서 쓰면 생성되는 객체(object)를 뜻한다.
+}; // 이렇게 constructor안에서 생성되는 객체를 instance라고 한다.
+
+var 오브젝트 = new 기계(); // 기계 {이름: 'Kim'}
+
+document.querySelector("#버튼").addEventListener("click", function (e) {
+ console.log(this); // e.currentTarget
+}); // 이 함수에서 화살표함수를 쓰면 this는 {window}로 출력된다.
+
+function makeArray (e) {
+    var 어레이 = [1,2,3];
+    어레이.forEach(function (v) {
+        console.log(this); // {window}
+    });
+};
+
+makeArray();
+
+var 객체4 = {
+    이름들 : ["김", "이", "박"],
+    함수 : function () {
+        console.log(this); // 해당 object를 출력한다. {이름들: Array(3), 함수: ƒ}
+        객체4.이름들.forEach(function () {
+            console.log(this); // {window} --> this를 포함하는 {}가 객체가 아닌 일반함수이므로 window가 출력된다.
+        }); // 단, forEach의 callback함수를 화살표함수로 쓰게 되면 상위의 this값을 물려받으므로 object가 출력된다. {이름들: Array(3), 함수: ƒ}
+    },
+};
+
+객체4.함수(); 
