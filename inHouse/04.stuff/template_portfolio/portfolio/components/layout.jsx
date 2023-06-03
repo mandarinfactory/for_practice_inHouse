581d4c7be17fa4039f7c45ec0dcc3066d2750063
Ho Jun Lee <hoojunee@gmail.com>
2023-06-03 23:54:23 +0900

    23.06.03
    portpolio_다시시작
    전체적인layout설계함(헤더,푸터)

--- inHouse/04.stuff/template_portfolio/portfolio/components/layout.jsx
+++ inHouse/04.stuff/template_portfolio/portfolio/components/layout.jsx
@@ -2,12 +2,14 @@ import Header from "./header";
 import Footer from "./footer";
 
 const Layout = ({ children }) => {
-    return (
-        <>
-            <h1>레이아웃</h1>
-            <div>{children}</div>
-        </>
-    );
+  return (
+    <>
+      <Header />
+      <h1>레이아웃</h1>
+      <div>{children}</div>
+      <Footer />
+    </>
+  );
 };
 
 export default Layout;
