fb92890c4c3807505ee197cae928860206e8034d
Ho Jun Lee <hoojunee@gmail.com>
2023-06-04 14:06:36 +0900

    23.06.04
    portpolio_다시시작
    header, footer tailblocks library 이용해서 디자인함

--- inHouse/04.stuff/template_portfolio/app/page.js
+++ inHouse/04.stuff/template_portfolio/app/page.js
@@ -4,7 +4,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <Layout>
-      <h1 className="text-3xl font-bold underline">홈입니다.</h1>
+      <h1>홈입니다.</h1>
     </Layout>
   );
 };
