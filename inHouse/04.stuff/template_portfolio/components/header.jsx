28facd0abbea0393238a2daa4fc5fd6372e74073
Ho Jun Lee <hoojunee@gmail.com>
2023-06-04 14:59:11 +0900

    23.06.04
    portpolio_다시시작
    link랑 a 호환불가 해결

--- inHouse/04.stuff/template_portfolio/components/header.jsx
+++ inHouse/04.stuff/template_portfolio/components/header.jsx
@@ -1,9 +1,11 @@
+import Link from "next/link";
+
 export default function Header() {
   return (
     <>
-      <header class="text-gray-600 body-font">
-        <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
-          <a class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
+      <header className="text-gray-600 body-font">
+        <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+          <a className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -11,19 +13,24 @@ export default function Header() {
               stroke-linecap="round"
               stroke-linejoin="round"
               stroke-width="2"
-              class="w-10 h-10 text-white p-2 bg-yellow-500 rounded-full"
+              className="w-10 h-10 text-white p-2 bg-yellow-500 rounded-full"
               viewBox="0 0 24 24"
             >
               <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
             </svg>
-            <span class="ml-3 text-xl">이호준의 포트폴리오</span>
+            <span className="ml-3 text-xl">이호준의 포트폴리오</span>
           </a>
-          <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-            <a class="mr-5 hover:text-gray-900">홈</a>
-            <a class="mr-5 hover:text-gray-900">프로젝트</a>
-            <a class="mr-5 hover:text-gray-900">연락하기</a>
+          <nav className="md:ml-auto flex flex-wrap items-center text-base justify-center">
+            <a className="mr-5 hover:text-gray-900">
+              홈
+            </a>
+            <a className="mr-5 hover:text-gray-900">About Me</a>
+            <Link href="/projects">
+              <a className="mr-5 hover:text-gray-900">프로젝트</a>
+            </Link>
+            <a className="mr-5 hover:text-gray-900">연락하기</a>
           </nav>
-          <button class="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
+          <button className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
             Button
             <svg
               fill="none"
@@ -31,7 +38,7 @@ export default function Header() {
               stroke-linecap="round"
               stroke-linejoin="round"
               stroke-width="2"
-              class="w-4 h-4 ml-1"
+              className="w-4 h-4 ml-1"
               viewBox="0 0 24 24"
             >
               <path d="M5 12h14M12 5l7 7-7 7"></path>
