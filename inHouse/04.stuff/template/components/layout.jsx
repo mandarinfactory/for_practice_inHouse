d713b701315fe69eecbe0aaea7eab573d6f95c93
Ho Jun Lee <hoojunee@gmail.com>
2023-06-05 23:59:45 +0900

    23.06.05
    portfolio
    다시시작...ㅂㄷ
    pages가 아닌 app기반으로 만들어서 다시 시작함..

--- inHouse/04.stuff/template/components/layout.jsx
+++ inHouse/04.stuff/template/components/layout.jsx
@@ -1,7 +1,13 @@
-export default function Layout ({ children }) {
-    return (
-        <>
-            <div>{children}</div>
-        </>
-    );
-};
+import Footer from "./footer";
+import Header from "./header";
+
+export default function Layout({ children }) {
+  return (
+    <>
+      <Header />
+      <h1>레이아웃입니다.</h1>
+      <div>{children}</div>
+      <Footer />
+    </>
+  );
+}
