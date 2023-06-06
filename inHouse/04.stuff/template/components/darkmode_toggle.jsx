08d063b0dc5cd97935d2f2bac14b33a42b5ad063
Ho Jun Lee <hoojunee@gmail.com>
2023-06-06 23:17:39 +0900

    23.06.06
    portfolio
    darkmode 설정함

--- inHouse/04.stuff/template/components/darkmode_toggle.jsx
+++ inHouse/04.stuff/template/components/darkmode_toggle.jsx
@@ -5,7 +5,18 @@ export default function DarkModeToggleButton() {
 
   return (
     <button
-      className="inline-flex items-center bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0"
+      className="inline-flex items-center 
+      bg-gray-200 border-0 py-1 px-3 
+      focus:outline-none 
+      hover:bg-gray-300
+      hover:text-orange-500 
+      dark:text-slate-400
+      dark:bg-slate-600
+      hover:dark:bg-slate-700
+      dark:hover:text-yellow-400
+      rounded text-base mt-4 md:mt-0
+      duration-100
+      "
       type="button"
       onClick={() => {
         setTheme(theme === "dark" ? "light" : "dark");
@@ -16,7 +27,7 @@ export default function DarkModeToggleButton() {
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
         fill="currentColor"
-        className="visible dark:invisible w-6 h-6 dark:w-0 dark:h-0 text-orange-500 duration-200"
+        className="visible dark:invisible w-6 h-6 dark:w-0 dark:h-0"
       >
         <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z" />
       </svg>
@@ -26,7 +37,7 @@ export default function DarkModeToggleButton() {
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 24 24"
         fill="currentColor"
-        className="invisible dark:visible w-0 h-0 dark:w-6 dark:h-6 text-yellow-400 duration-200"
+        className="invisible dark:visible w-0 h-0 dark:w-6 dark:h-6"
       >
         <path
           fill-rule="evenodd"
--- inHouse/04.stuff/template/components/layout.jsx
+++ inHouse/04.stuff/template/components/layout.jsx
@@ -3,11 +3,11 @@ import Header from "./header";
 
 export default function Layout({ children }) {
   return (
-    <>
+    <div className="bg-primary">
       <Header />
       <h1>레이아웃입니다.</h1>
       <div>{children}</div>
       <Footer />
-    </>
+    </div>
   );
 };
