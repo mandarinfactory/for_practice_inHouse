1ba9df45d2154fd685119450617e144cd4fbede4
Ho Jun Lee <hoojunee@gmail.com>
2023-06-07 21:18:37 +0900

    23.06.06
    portfolio
    next JS로 data fetching하기(Notion API)

---
+++ inHouse/04.stuff/template/config/index.js
@@ -0,0 +1,1 @@
+export const DATABASE_ID = process.env;
--- inHouse/04.stuff/template/pages/Projects.jsx
+++ inHouse/04.stuff/template/pages/Projects.jsx
@@ -1,9 +1,30 @@
 import Layout from "@/components/layout";
 
-export default function Projects () {
-    return (
-        <Layout>
-            <h1>프로젝트입니다.</h1>
-        </Layout>
-    );
-};
+export default function Projects() {
+  return (
+    <Layout>
+      <h1>프로젝트입니다.</h1>
+    </Layout>
+  );
+}
+
+//build-time에 호출되서 data를 가지고 온 후에 화면에 나타낸다.
+export async function getStaticProps(context) {
+
+  const options = {
+    method: "POST",
+    headers: {
+      Accept: "application/json",
+      "Notion-Version": "2022-06-28",
+      "content-type": "application/json",
+      "Authoriztion"
+    },
+    body: JSON.stringify({ page_size: 100 }),
+  };
+
+  fetch("https://api.notion.com/v1/databases/database_id/query", options)
+
+  return {
+    props: {},
+  };
+}
