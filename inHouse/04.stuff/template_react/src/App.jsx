9752e73d74ab00567394186127058f5e5aa1c9f5
Ho Jun Lee <hoojunee@gmail.com>
2023-06-10 16:18:10 +0900

    23.06.10
    template_react 다시시작

---
+++ inHouse/04.stuff/template_react/src/App.jsx
@@ -0,0 +1,9 @@
+import './index.css'
+
+export default function App() {
+  return (
+    <>
+      <h1 className="text-red-600 font-extrabold text-3xl">이것이 React + Vite + MAP</h1>
+    </>
+  );
+};
---
+++ inHouse/04.stuff/template_react/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  darkMode: "class",
+  theme: {
+    fontFamily: {
+      "sans" : ["NanumSquare-Neo"],
+    },
+  },
+  plugins: [],
+}
