07d2376e4816d9cde4f0f5eb8a8b42c005c342d9
Ho Jun Lee <hoojunee@gmail.com>
2023-06-13 00:35:37 +0900

    23.06.12
    TS 초급 : JS의 class, prototype 알아보기

---
+++ inHouse/03.TypeScript/03/app.js
@@ -0,0 +1,23 @@
+/* JS class문법 && prototype을 알아보자 */
+
+function 기계 (Qskill, Wskill) {
+    this.q = Qskill,
+    this.w = Wskill
+}
+// class는 쉽게말해 object를 만드는 기계이다. --> 여기서 만들어지는 object를 instance라고 한다.
+
+var a = new 기계("consume", "snowball");
+console.log(a); // 기계 {q: 'consume', w: 'snowball'}
+
+class Hero {
+    Constructor () {
+        this.q = "가나다";
+        this.w = "라마바";
+    };
+}; // ES6오면서 따로 class라고 명령어가 생겼다.
+
+console.log(기계.prototype); // {constructor: ƒ} --> prototype은 유전자라고 생각하면 쉽다!
+// 따라서 prototype에 뭔가 추가하면 해당 자식들도 사용이 가능하다. --> 상속이 된다.
+
+기계.prototype.name = "Brent";
+console.log(a.name); // Brent
