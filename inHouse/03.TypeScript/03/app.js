8b4b2ff118a191684880560bfbf394545281ee52
Ho Jun Lee <hoojunee@gmail.com>
2023-06-13 22:17:43 +0900

    23.06.12
    TS 초급 : object type 지정시 쓸수 있는 interface 활용방법

--- inHouse/03.TypeScript/03/app.js
+++ inHouse/03.TypeScript/03/app.js
@@ -1,17 +1,20 @@
+/* 07. class만들때 type지정 및 object type 지정시에 interface사용 */
 class Person {
     name;
     // 여기를 field값이라고 하고 사전에 field값이 있고 type까지 지정이 되어있어야
     // constructor에서 사용이 가능하다.
     constructor(a) {
+        // parameter에 직접 type을 지정한다.
         this.name = a;
     }
-    ;
-    함수(b) {
+    함수12(b) {
+        // prototype에 들어가는 함수12에도 type을 지정한다.
         console.log("안녕" + b);
     }
-    ;
 }
-;
 let 사람1 = new Person("Brent");
 let 사람2 = new Person("John");
-사람1.함수("반가워");
+사람1.함수12("반가워");
+let 네모 = { color: "red", width: 100 };
+let 학생 = { name: "kim" };
+let 선생 = { name: "kim", age: 20 };
--- inHouse/03.TypeScript/03/app.ts
+++ inHouse/03.TypeScript/03/app.ts
@@ -1,16 +1,38 @@
+/* 07. class만들때 type지정 및 object type 지정시에 interface사용 */
+
 class Person {
-    name :string; 
-    // 여기를 field값이라고 하고 사전에 field값이 있고 type까지 지정이 되어있어야
-    // constructor에서 사용이 가능하다.
-    constructor (a:string) { // parameter에 직접 type을 지정한다.
-        this.name = a;
-    };
-    함수(b :string) { // prototype에 들어가는 함수에도 type을 지정한다.
-        console.log("안녕" + b);
-        
-    };
-};
+  name: string;
+  // 여기를 field값이라고 하고 사전에 field값이 있고 type까지 지정이 되어있어야
+  // constructor에서 사용이 가능하다.
+  constructor(a: string) {
+    // parameter에 직접 type을 지정한다.
+    this.name = a;
+  }
+  함수12(b: string) {
+    // prototype에 들어가는 함수12에도 type을 지정한다.
+    console.log("안녕" + b);
+  }
+}
 
 let 사람1 = new Person("Brent");
 let 사람2 = new Person("John");
-사람1.함수("반가워");
+사람1.함수12("반가워");
+
+interface Square {
+  color: string;
+  width: number;
+} // interface는 class 만드는거랑 유사하다.
+let 네모: Square = { color: "red", width: 100 };
+
+interface Student {
+  name: string;
+}
+interface Teacher extends Student {
+  age: number;
+}
+let 학생: Student = { name: "kim" };
+let 선생: Teacher = { name: "kim", age: 20 };
+/* interface를 쓰면 extends(복사붙여넣기 명령어이다.)로 복사가 가능하다. */
+
+type Animal = { name: string };
+type Cat = { age: number } & Animal; // extends와 같이 type을 쓸때는 &(intersection type)기호를 쓰면 된다.
