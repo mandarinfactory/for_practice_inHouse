b7e5e319433489de1363217626e3f087b48c8fb1
Ho Jun Lee <hoojunee@gmail.com>
2023-06-15 00:20:37 +0900

    23.06.12
    TS 초급 : public, private

--- inHouse/03.TypeScript/04/app.js
+++ inHouse/03.TypeScript/04/app.js
@@ -57,6 +57,8 @@ function 함수7() {
 class User {
     name = "kim"; // -public이 붙으면 모든 자식들이 사용가능해 진다. (있으나 없으나 큰 차이가 없다. --> 강제부여됨)
     age = 29; // private이 붙으면 자식들이 수정할 수 없다.
+    familyName = "몰루?";
+    theName;
     constructor(a) {
         this.name = a;
     }
@@ -65,4 +67,3 @@ class User {
 ;
 let 유저1 = new User("park");
 유저1.name = "안녕?";
-유저1.age;
