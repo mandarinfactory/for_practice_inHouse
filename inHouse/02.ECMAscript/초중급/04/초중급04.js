40783fe4e21e8a257020fcbf68bbb3e9066f7837
Ho Jun Lee <hoojunee@gmail.com>
2023-06-20 23:32:07 +0900

    23.06.20
    JS 초중급 : stack, queue방을 알아보자

--- inHouse/02.ECMAscript/초중급/04/초중급04.js
+++ inHouse/02.ECMAscript/초중급/04/초중급04.js
@@ -54,3 +54,20 @@ export {a, b};
 
 export default c;
 // export를 쓰고 export default를 동시에 쓸수 있다.
+
+// 13. Stack, Queue를 이용한 웹브라우저 동작원리
+
+console.log(1+1);
+setTimeout(function () {
+  console.log(2+2);
+}, 1000)
+console.log(3+3); // 2 6 출력되고 1초 후에 4가 출력된다.
+/* 
+웹브라우저는 Heap, Stack, Queue라는 방들이 있다.
+Heap에는 만들었던 변수들이 저장되어 있는 방이고 Stack에서 코드가 실행되는 방이다. --> 여기서 코드 한줄씩만 처리된다.(single thread)
+위 setTimeout같은 함수들(Ajax요청코드, eventListener 등)은 대기실방에 들어가서 대기한다.
+대기실에서 조건이 맞춰진 함수들은 다시 Queue라는 대기실로 이동한다. 여기서 순차적으로 한개씩 Stack으로 올라간다.
+Stack으로 올라갈때 Stack방은 텅 비어있어야 Queue대기실에 있던 함수들이 올라간다.
+따라서 먼저 Stack에서 바로 출력될 수 있는 2 6이 먼저 출력되고 4가 1초후에 출력(setTimeout함수)되는 구조이다.
+설령 setTimeout이 0초여도 일단 대기실로 들어가기때문에 2 6 4순으로 출력된다.
+*/
