688e5d4fd8d4d8c2fbd75a0c18a8b9d9103488e9
Ho Jun Lee <hoojunee@gmail.com>
2023-07-09 16:21:34 +0900

    23.07.09
    react_project2  :  검색component완성 및 맵component 수정예정

--- inHouse/04.stuff/template_react2/src/components/screen/DetailMovieInfos.jsx
+++ inHouse/04.stuff/template_react2/src/components/screen/DetailMovieInfos.jsx
@@ -1,10 +1,26 @@
-import { useEffect } from "react";
+export default function DetailMovieInfos({ movieVal, setDetailMovieInfos }) {
+  console.log(movieVal);
+  const actorsName = [];
+  movieVal.actors.actor.forEach((v) => {
+    actorsName.push(`${v.actorNm}, `);
+  });
+  actorsName.push(actorsName[actorsName.length - 1].replace(/,?/gi, "")); // 마지막에 오는 문자열에 있는 ,을 없애주고 추가해서
+  actorsName.splice(actorsName.length - 2, 1); // 계속,이 붙어있는 문자열은 제거해줌.
 
-export default function DetailMovieInfos({ movieVal }) {
   return (
-    <div className="absolute w-[50%] h-[80%] bg-slate-100 shadow-lg rounded-xl">
+    <div className="flex flex-col justify-center items-center absolute w-[35%] h-full bg-slate-50 shadow-lg rounded-xl">
+      <button onClick={() => setDetailMovieInfos(false)}>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="3em"
+          viewBox="0 0 384 512"
+          className="absolute top-[10px] right-[10px]"
+        >
+          <path d="M342.6 150.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0L192 210.7 86.6 105.4c-12.5-12.5-32.8-12.5-45.3 0s-12.5 32.8 0 45.3L146.7 256 41.4 361.4c-12.5 12.5-12.5 32.8 0 45.3s32.8 12.5 45.3 0L192 301.3 297.4 406.6c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L237.3 256 342.6 150.6z" />
+        </svg>
+      </button>
       <img
-        className="w-auto h-auto"
+        className="w-[40%] h-auto my-10"
         src={
           movieVal.posters.includes("|")
             ? movieVal.posters.substring(0, movieVal.posters.indexOf("|"))
@@ -12,11 +28,22 @@ export default function DetailMovieInfos({ movieVal }) {
         }
         alt="movie-poster"
       />
-      <h1 className="text-2xl font-bold">
+      <h1 className="text-3xl font-bold my-3">
         {movieVal.title.includes("!")
           ? movieVal.title.replace(/!HS?E?/gi, "")
           : movieVal.title}
       </h1>
+      <p className="my-1 text-lg font-bold">감독 : {movieVal.directors.director[0].directorNm}</p>
+      <p className="flex justify-center w-[70%] my-1 text-lg font-bold">배우 : {actorsName}</p>
+      <p className="my-1">
+        {movieVal.ratings.rating[0].ratingGrade.includes("|")
+          ? movieVal.ratings.rating[0].ratingGrade.substring(
+              0,
+              movieVal.ratings.rating[0].ratingGrade.indexOf("|")
+            )
+          : movieVal.ratings.rating[0].ratingGrade}
+      </p>
+      <p className="w-[70%] my-3">{movieVal.plots.plot[0].plotText}</p>
     </div>
   );
 }
--- inHouse/04.stuff/template_react2/src/components/screen/Hero.jsx
+++ inHouse/04.stuff/template_react2/src/components/screen/Hero.jsx
@@ -91,7 +91,7 @@ export default function Hero({
         <></>
       )}
       <CinemaMap />
-      {detailMovieInfos ? <DetailMovieInfos movieVal={movieVal} /> : <></>}
+      {detailMovieInfos ? <DetailMovieInfos movieVal={movieVal} setDetailMovieInfos={setDetailMovieInfos}/> : <></>}
     </div>
   );
 }
