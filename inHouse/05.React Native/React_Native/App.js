49c70bb77579e43338c6103bbcf2042d9db08469
Ho Jun Lee <hoojunee@gmail.com>
2023-08-12 15:56:52 +0900

    23.08.12
    React Native 다시 정리

--- inHouse/05.React Native/React_Native/App.js
+++ inHouse/05.React Native/React_Native/App.js
@@ -1,79 +1,14 @@
 import { useState } from "react";
 import { StyleSheet, View, FlatList, Button } from "react-native";
 
-import GoalItem from "./components/GoalItem";
-import GoalInput from "./components/GoalInput";
-
 export default function App() {
-  const [courseGoals, setCourseGoals] = useState([]);
-  const [modalIsVisible, setModalIsVisible] = useState(false);
-
-  const addGoalHandler = (enteredGoalText) => {
-    setCourseGoals((currentCourseGoals) => [
-      ...currentCourseGoals,
-      { text: enteredGoalText, id: Math.random().toString() },
-    ]);
-    endGoalModalHandler();
-  };
-
-  const deleteGoalItems = (id) => {
-    setCourseGoals((currentCourseGoals) => {
-      return currentCourseGoals.filter((goal) => {
-        goal.id !== id;
-      });
-    });
-  };
-
-  const startGoalModalHandler = () => {
-    setModalIsVisible(true);
-  };
-
-  const endGoalModalHandler = () => {
-    setModalIsVisible(false);
-  };
-
+  
   return (
-    <View style={styles.appContainer}>
-      <Button
-        title="새로운 목표 추가"
-        color="#1877f2"
-        onPress={startGoalModalHandler}
-      />
-      <GoalInput
-        visible={modalIsVisible}
-        addGoalHandler={addGoalHandler}
-        endGoalModalHandler={endGoalModalHandler}
-      />
-      <View style={styles.goalsContainer}>
-        <FlatList
-          data={courseGoals}
-          renderItem={(itemData) => {
-            return (
-              <GoalItem
-                text={itemData.item.text}
-                id={itemData.item.id}
-                onDeleteItem={deleteGoalItems}
-              />
-            );
-          }}
-          keyExtractor={(item, index) => {
-            return item.id;
-          }}
-          alwaysBounceVertical={false}
-        ></FlatList>
-      </View>
-    </View>
-  );
+    <>
+    </>
+  )
 }
 
 const styles = StyleSheet.create({
-  appContainer: {
-    flex: 1,
-    paddingTop: 50,
-    paddingHorizontal: 16,
-    backgroundColor: "#61dafb"
-  },
-  goalsContainer: {
-    flex: 5,
-  },
+  
 });
