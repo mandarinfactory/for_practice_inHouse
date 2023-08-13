992dd72d88d5c41f925e2a6dbb887bebb095ee16
Ho Jun Lee <hoojunee@gmail.com>
2023-08-13 11:38:48 +0900

    23.08.12
    React Native에서 Native들간의 같은style이지만 다르게 설정해야하는것들

--- inHouse/05.React Native/React_Native/components/PrimaryButton.jsx
+++ inHouse/05.React Native/React_Native/components/PrimaryButton.jsx
@@ -1,9 +1,40 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable, StyleSheet } from "react-native";
 
 export default function PrimaryButton({ children }) {
   return (
-    <View>
-      <Text>{children}</Text>
+    <View style={styles.btnOuterContainer}>
+      <Pressable
+        onPress={""}
+        android_ripple={{ color: "#640233" }}
+        style={({ pressed }) =>
+          pressed
+            ? [styles.btnInnerContainer, styles.pressed]
+            : styles.btnInnerContainer
+        }
+      >
+        <Text style={styles.btnText}>{children}</Text>
+      </Pressable>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  btnOuterContainer: {
+    margin: 5,
+    borderRadius: 30,
+    overflow: "hidden",
+  },
+  btnInnerContainer: {
+    backgroundColor: "#72063c",
+    paddingVertical: 10,
+    paddingHorizontal: 15,
+    elevation: 5,
+  },
+  btnText: {
+    color: "white",
+    textAlign: "center",
+  },
+  pressed: {
+    opacity: 0.75,
+  }, // iphone에서는 android_ripple이 따로 없으므로
+});
--- inHouse/05.React Native/React_Native/screens/StartGameScreen.jsx
+++ inHouse/05.React Native/React_Native/screens/StartGameScreen.jsx
@@ -4,7 +4,13 @@ import PrimaryButton from "../components/PrimaryButton";
 export default function StartGameScreen() {
   return (
     <View style={styles.inputContainer}>
-      <TextInput />
+      <TextInput
+        style={styles.textInput}
+        maxLength={2}
+        keyboardType="number-pad"
+        autoCapitalize="none"
+        autoCorrect={false}
+      />
       <PrimaryButton>리셋</PrimaryButton>
       <PrimaryButton>확인</PrimaryButton>
     </View>
@@ -12,16 +18,27 @@ export default function StartGameScreen() {
 }
 
 const styles = StyleSheet.create({
-    inputContainer : {
-        padding: 15,
-        marginTop: 100,
-        marginHorizontal: 25,
-        borderRadius: 13,
-        backgroundColor: "#72063c",
-        elevation: 5, // android box-shadow
-        shadowColor: "black", //iphone box-shadow(그림지의 색상)
-        shadowOffset: {width : 0,height: 2}, //iphone box-shadow(그림자의 크키, 우치)
-        shadowRadius: 7, //iphone box-shadow(그림자의 번짐의 크기)
-        shadowOpacity: 0.25, //iphone box-shadow(그림자의 투명도)
-    }, 
+  inputContainer: {
+    padding: 15,
+    marginTop: 100,
+    marginHorizontal: 25,
+    borderRadius: 13,
+    backgroundColor: "#4e0329",
+    elevation: 5, // android box-shadow
+    shadowColor: "black", //iphone box-shadow(그림지의 색상)
+    shadowOffset: { width: 0, height: 2 }, //iphone box-shadow(그림자의 크키, 우치)
+    shadowRadius: 7, //iphone box-shadow(그림자의 번짐의 크기)
+    shadowOpacity: 0.25, //iphone box-shadow(그림자의 투명도)
+  },
+  textInput: {
+    width: 50,
+    height: 50,
+    fontSize: 35,
+    borderBottomColor: "#ddb52f",
+    borderBottomWidth: 3,
+    color: "#ddb52f",
+    marginVertical: 9,
+    fontWeight: "bold",
+    textAlign: "center",
+  },
 });
