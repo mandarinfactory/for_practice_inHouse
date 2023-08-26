1a1ae794301f90875c2e526518185d6c533ebfcd
Ho Jun Lee <hoojunee@gmail.com>
2023-08-26 15:46:52 +0900

    23.08.26
    새로운 프로젝트(navigation연습 등)

---
+++ inHouse/05.React Native/React_Native_03/screens/AllExpenses.jsx
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+export default function AllExpenses () {
+    return (
+        <Text>AllExpenses Screen!</Text>
+    );
+};
---
+++ inHouse/05.React Native/React_Native_03/screens/ManageExpense.jsx
@@ -0,0 +1,7 @@
+import { Text } from "react-native";
+
+export default function ManageExpense () {
+    return (
+        <Text>ManageExpense Screen!</Text>
+    );
+};
