1309dbc0ba2a9305a4be2a64e47434b7eb48d5dc
Ho Jun Lee <hoojunee@gmail.com>
2023-09-07 01:29:21 +0900

    23.09.03
    react 새로운 프로젝트 공공데이터 xml 파싱후 json변환성공(library이용함)

--- inHouse/05.React Native/template_03/API/index.js
+++ inHouse/05.React Native/template_03/API/index.js
@@ -3,7 +3,7 @@ const getAptTradeAPI = async () => {
     "http://openapi.molit.go.kr/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTradeDev";
   const API_KEY =
     "24vbFaV5oWpSo3qOGdwCXPO%2FX5gr4tqD2gxEwUJWb2xVcv4sWZ5QmmZruySMYWl2471GK88wVe3zjfacPH%2FENQ%3D%3D";
-  const headers = "&pageNo=1&numOfRows=10&LAWD_CD=11650&DEAL_YMD=202308";
+  const headers = "&pageNo=1&numOfRows=10&LAWD_CD=11620&DEAL_YMD=202308";
   const reqestAPI_URL = `${API_URL}?serviceKey=${API_KEY}${headers}`;
   const response = await fetch(reqestAPI_URL)
     .then((res) => res)
@@ -13,12 +13,15 @@ const getAptTradeAPI = async () => {
   const xmlString = await response.text();
   const parseString = require("react-native-xml2js").parseString;
   parseString(xmlString, (err, result) => {
-    if(err !== null) {
+    if (err !== null) {
       console.log("ERROR", err);
-      return
-    };
-    console.log(JSON.stringify(result));
-  })
+      return;
+    }
+    let rawData = JSON.stringify(result);
+    let JSONData = JSON.parse(rawData);
+    let aptData = JSONData.response.body[0].items[0].item;
+    console.log(aptData[4].지역코드);
+  });
 };
 
 export default getAptTradeAPI;
