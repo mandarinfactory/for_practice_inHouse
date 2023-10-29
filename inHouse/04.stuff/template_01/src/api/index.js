2a058e965fc9d0327c7391ec64731008b8562a58
Ho Jun Lee <hoojunee@gmail.com>
2023-10-30 00:13:36 +0900

    23.10.29
    그냥...베포할래 시부레...

--- inHouse/04.stuff/template_01/src/api/index.js
+++ inHouse/04.stuff/template_01/src/api/index.js
@@ -9,16 +9,18 @@ const fetchJson = async (url) => {
 };
 
 export const getBoxOfficeInfo = async (MovieInfosCtx) => {
-  const url = `http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=${import.meta.env.VITE_KOBIS_KEY}&targetDt=${
-    date - 1
-  }&itemPerPage=5`;
+  const url = `http://www.kobis.or.kr/kobisopenapi/webservice/rest/boxoffice/searchDailyBoxOfficeList.json?key=${
+    import.meta.env.VITE_KOBIS_KEY
+  }&targetDt=${date - 1}&itemPerPage=5`;
   const json = await fetchJson(url);
   MovieInfosCtx.setMoviesInfo(json?.boxOfficeResult?.dailyBoxOfficeList);
   MovieInfosCtx.setIsLoading(false);
 };
 
 export const getSearchMovieInfo = async (MovieInfosCtx, setSearchedMovie) => {
-  const url = `http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&ServiceKey=${import.meta.env.VITE_KMDB_KEY}&query=${MovieInfosCtx.searchMovieKeyword}`;
+  const url = `http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&ServiceKey=${
+    import.meta.env.VITE_KMDB_KEY
+  }&query=${MovieInfosCtx.searchMovieKeyword}`;
   const json = await fetchJson(url);
 
   if (MovieInfosCtx.searchMovieKeyword) {
@@ -37,14 +39,18 @@ export const getSearchGenreMovies = async (
   MovieInfosCtx,
   genres
 ) => {
-  const url = `http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&ServiceKey=${import.meta.env.VITE_KMDB_KEY}&genre=${genres}&releaseDts="20100101`;
+  const url = `http://api.koreafilm.or.kr/openapi-data2/wisenut/search_api/search_json2.jsp?collection=kmdb_new2&ServiceKey=${
+    import.meta.env.VITE_KMDB_KEY
+  }&genre=${genres}&releaseDts="20100101`;
   const json = await fetchJson(url);
   setCuratedMovie(json.Data[0].Result);
   MovieInfosCtx.setIsLoading(false);
 };
 
 export const getUpComingMovies = async (setUpcomings, MovieInfosCtx) => {
-  const url = `https://api.themoviedb.org/3/movie/upcoming?api_key=${import.meta.env.VITE_TMDB_KEY}&language=ko-KR&region=KR&page=1`;
+  const url = `https://api.themoviedb.org/3/movie/upcoming?api_key=${
+    import.meta.env.VITE_TMDB_KEY
+  }&language=ko-KR&region=KR&page=1`;
   const json = await fetchJson(url);
   setUpcomings(json.results);
   MovieInfosCtx.setIsLoading(false);
