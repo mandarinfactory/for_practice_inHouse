3e6619764e806634ed98cc5c5feec15b6ec7c116
Ho Jun Lee <hoojunee@gmail.com>
2023-11-17 01:23:45 +0900

    23.11.17
    THREE.js 연습

--- inHouse/02.ECMAscript/three.js/apps.js
+++ inHouse/02.ECMAscript/three.js/apps.js
@@ -1,16 +1,25 @@
 import * as THREE from "./node_modules/three/build/three.module.js";
 
 const scene = new THREE.Scene(); // 장면
-scene.background = new THREE.Color(0x004fff);
 const camera = new THREE.PerspectiveCamera(
   75,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
 ); // 카메라
-const renderer = new THREE.WebGL1Renderer(); // 렌더러
+const canvas = document.querySelector(".ex-03"); // 캔버스
+const renderer = new THREE.WebGL1Renderer({canvas}); // 렌더러
 renderer.setSize(window.innerWidth, window.innerHeight);
 
 document.body.appendChild(renderer.domElement);
 
-renderer.render(scene, camera); // 렌더러 내에 장면과 카메라를 추가
+const render = (time) => {
+  time *= 0.001;
+  //cube.rotation.x = time;
+  //cube.rotation.y = time;
+
+  renderer.render(scene, camera); // 렌더러 내에 장면과 카메라를 추가
+
+  requestAnimationFrame(render);
+};
+requestAnimationFrame(render);
