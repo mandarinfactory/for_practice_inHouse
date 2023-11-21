fd6a3da4e70a2837bc41c1c6fbf3e293d32c4a28
Ho Jun Lee <hoojunee@gmail.com>
2023-11-22 00:26:20 +0900

    23.11.16
    프로젝트3 hard-coding 및 기타 구현중

--- inHouse/04.stuff/template_03/src/components/screens/Header.jsx
+++ inHouse/04.stuff/template_03/src/components/screens/Header.jsx
@@ -24,7 +24,7 @@ export default function Header() {
 
   return (
     <header className="text-white bg-teal-700 body-font">
-      <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+      <div className="container mx-auto flex flex-wrap p-5 lg:flex-row md:flex-row sm:flex-col justify-between sm:justify-center items-center">
         <a
           className="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
           href="./"
