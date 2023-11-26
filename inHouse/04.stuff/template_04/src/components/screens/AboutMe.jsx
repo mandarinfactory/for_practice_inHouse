a91d5ea648bbec4f0ba85461098ca81d57f5b48e
Ho Jun Lee <hoojunee@gmail.com>
2023-11-26 18:36:51 +0900

    23.11.26
    포트폴리오 지워져서 다시 만들고 있음

--- inHouse/04.stuff/template_04/src/components/screens/AboutMe.jsx
+++ inHouse/04.stuff/template_04/src/components/screens/AboutMe.jsx
@@ -32,34 +32,34 @@ export default function AboutMe() {
             </p>
           </div>
           <div className="mt-4">
-            <h1 className="my-5 text-2xl">연락처</h1>
+            <h1 className="my-5 text-2xl">연락처 및 이력서</h1>
             <div className="w-full h-auto flex">
-              <a href="" title="휴대전화">
+              <a href="tel:+8201098370037" title="휴대전화">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   height="3em"
                   viewBox="0 0 384 512"
-                  className="mr-10 fill-gray-700 hover:fill-black cursor-pointer duration-100"
+                  className="mr-10 fill-gray-600 dark:fill-gray-300 hover:fill-black dark:hover:fill-gray-100 cursor-pointer duration-100"
                 >
                   <path d="M80 0C44.7 0 16 28.7 16 64V448c0 35.3 28.7 64 64 64H304c35.3 0 64-28.7 64-64V64c0-35.3-28.7-64-64-64H80zm80 432h64c8.8 0 16 7.2 16 16s-7.2 16-16 16H160c-8.8 0-16-7.2-16-16s7.2-16 16-16z" />
                 </svg>
               </a>
-              <a href="" title="이메일">
+              <a href="mailto:hoojunee@gmail.com" title="이메일">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   height="3em"
                   viewBox="0 0 512 512"
-                  className="mr-10 fill-gray-700 hover:fill-black cursor-pointer duration-100"
+                  className="mr-10 fill-gray-600 dark:fill-gray-300 hover:fill-black dark:hover:fill-gray-100 cursor-pointer duration-100"
                   >
                   <path d="M48 64C21.5 64 0 85.5 0 112c0 15.1 7.1 29.3 19.2 38.4L236.8 313.6c11.4 8.5 27 8.5 38.4 0L492.8 150.4c12.1-9.1 19.2-23.3 19.2-38.4c0-26.5-21.5-48-48-48H48zM0 176V384c0 35.3 28.7 64 64 64H448c35.3 0 64-28.7 64-64V176L294.4 339.2c-22.8 17.1-54 17.1-76.8 0L0 176z" />
                 </svg>
               </a>
-              <a href="" title="이력서">
+              <a href="../../file/이력서.pdf" download title="이력서">
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   height="3em"
                   viewBox="0 0 384 512"
-                  className="fill-gray-700 hover:fill-black cursor-pointer duration-100"
+                  className="fill-gray-600 dark:fill-gray-300 hover:fill-black dark:hover:fill-gray-100 cursor-pointer duration-100"
                 >
                   <path d="M64 0C28.7 0 0 28.7 0 64V448c0 35.3 28.7 64 64 64H320c35.3 0 64-28.7 64-64V160H256c-17.7 0-32-14.3-32-32V0H64zM256 0V128H384L256 0zM112 256H272c8.8 0 16 7.2 16 16s-7.2 16-16 16H112c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64H272c8.8 0 16 7.2 16 16s-7.2 16-16 16H112c-8.8 0-16-7.2-16-16s7.2-16 16-16zm0 64H272c8.8 0 16 7.2 16 16s-7.2 16-16 16H112c-8.8 0-16-7.2-16-16s7.2-16 16-16z" />
                 </svg>
