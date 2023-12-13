b3453b92565c2ebe933171047785d9deda9b6929
Ho Jun Lee <hoojunee@gmail.com>
2023-12-13 23:25:12 +0900

    23.12.13
    타입스크립트, 리액트 연습 --> 유투브클론코딩

--- inHouse/04.stuff/template_05/src/components/layout/PageHeader.tsx
+++ inHouse/04.stuff/template_05/src/components/layout/PageHeader.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import { Menu } from "lucide-react";
+import logo from "../../assets/logo.jpg";
 
 const PageHeader = () => {
   return (
-    <div>PageHeader</div>
-  )
-}
+    <div className="flex lg:gap-20 gap-10 justify-between">
+      <div className="flex gap-4 items-center flex-shrink-0">
+        <button>
+          <Menu />
+        </button>
+        <a href="/">
+          <img className="h-[50px]" src={logo} alt="" />
+        </a>
+      </div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
 
-export default PageHeader
+export default PageHeader;
