af615b519d046ff2fd43106d0d722cfe5ba4124b
Ho Jun Lee <hoojunee@gmail.com>
2024-01-19 17:34:52 +0900

    24.01.19
    다시 정신차려서 프로젝트 시작!

---
+++ inHouse/04.stuff/template_05/src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Hero from "./components/Hero";
+
+const App:React.FC = () => {
+
+  return (
+    <>
+      <Header />
+      <Hero />
+      <Footer />
+    </>
+  )
+}
+
+export default App;
