9dc41aa45e5e6eac9b22f8d17af64e284a820fa7
Ho Jun Lee <hoojunee@gmail.com>
2024-04-19 00:06:15 +0900

    24.04.19
    vue.js기초공부

--- inHouse/06.Vue.js/vue-project/src/App.vue
+++ inHouse/06.Vue.js/vue-project/src/App.vue
@@ -1,58 +1,65 @@
 <script setup>
-import { ref } from "vue";
+import q from "./data/quizes.json";
+import { ref, watch } from "vue";
+import Card from "./components/Card.vue";
 
-const showModal = ref(false);
-const newNote = ref("");
-const notes = ref([]);
+const quizes = ref(q);
+const search = ref("");
 
-const getRandomLightColor = () => {
-  return "hsl(" + Math.random() * 360 + ", 100%, 75%)";
-};
-
-const addNote = () => {
-  if (newNote.value > 10)
-  notes.value.push({
-    id: Math.floor(Math.random() * 10000),
-    text: newNote.value,
-    date: new Date(),
-    backgroundColor: getRandomLightColor(),
-  });
-  showModal.value = false;
-  newNote.value = [];
-};
+watch(search, (v) => {
+  quizes.value = q.filter((quiz) =>
+    quiz.name.toLowerCase().includes(search.value.toLowerCase())
+  );
+});
 </script>
 
 <template>
-  <main>
-    <div v-if="showModal" class="overlay">
-      <div class="modal">
-        <textarea
-          v-model="newNote"
-          name="note"
-          id="note"
-          cols="30"
-          rows="10"
-        ></textarea>
-        <button @click="addNote">Add Note</button>
-        <button class="close" @click="showModal = false">Close</button>
-      </div>
-    </div>
-    <div class="container">
-      <header>
-        <h1>Notes</h1>
-        <button @click="showModal = true">+</button>
-      </header>
-      <div class="cardsContainer">
-        <div
-          v-for="note in notes"
-          :key="note.id"
-          class="card"
-          :style="{ backgroundColor: note.backgroundColor }"
-        >
-          <p class="main-text">{{ note.text }}</p>
-          <p class="date">{{ note.date.toLocaleDateString("ko-KR") }}</p>
+  <div class="container">
+    <header>
+      <h1>Quizes</h1>
+      <input v-model.trim="search" type="text" placeholder="Search..." />
+    </header>
+    <div class="options-container">
+      <Card v-for="quiz in quizes" :key="quiz.id" :quiz="quiz" />
+      <!-- <div v-for="quiz in quizes" :key="quiz.id" class="card">
+        <img :src="quiz.img" alt="" />
+        <div class="card-text">
+          <h2>{{ quiz.name }}</h2>
+          <p>{{ quiz.questions.name }} questions</p>
         </div>
-      </div>
+      </div> -->
     </div>
-  </main>
+  </div>
 </template>
+
+<style scoped>
+.container {
+  max-width: 1000px;
+  margin: 0 auto;
+}
+
+header {
+  margin-bottom: 10px;
+  margin-top: 30px;
+  display: flex;
+  align-items: center;
+}
+
+header h1 {
+  font-weight: bold;
+  margin-right: 30px;
+}
+
+header input {
+  border: none;
+  background-color: rgba(128, 128, 128, 0.1);
+  padding: 10px;
+  border-radius: 5px;
+}
+
+.options-container {
+  display: flex;
+  flex-wrap: wrap;
+  margin-top: 40px;
+}
+</style>
