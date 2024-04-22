c64786cf5e424dc2ad853f242d4cf17a8354af13
Ho Jun Lee <hoojunee@gmail.com>
2024-04-22 23:20:18 +0900

    24.04.22
    vuejs state: ref, reactive

--- inHouse/06.Vue.js/vue-state/src/App.vue
+++ inHouse/06.Vue.js/vue-state/src/App.vue
@@ -1,13 +1,49 @@
 <script setup>
+import { ref, reactive } from "vue";
 
+/* const red = ref(0);
+const blue = ref(0);
+const green = ref(0); */
+
+/* const color = ref({
+  red: 0,
+  blue: 0,
+  green: 0,
+}); */
+
+const color = reactive({
+  red: 0,
+  blue: 0,
+  green: 0,
+});
 </script>
 
 <template>
- <main>
-
- </main>
+  <main
+    :style="{
+      backgroundColor: `rgb(${color.red}, ${color.green}, ${color.blue})`,
+    }"
+  >
+    <div>
+      <input type="number" v-model="color.red" placeholder="red" />
+      <input type="number" v-model="color.blue" placeholder="blue" />
+      <input type="number" v-model="color.green" placeholder="green" />
+    </div>
+  </main>
 </template>
 
 <style scoped>
-
+main {
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+div {
+  display: flex;
+  flex-direction: column;
+}
+f input {
+  margin-bottom: 10px;
+}
 </style>
