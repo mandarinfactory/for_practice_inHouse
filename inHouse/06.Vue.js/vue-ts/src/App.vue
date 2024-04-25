c76be64b811b04535de60e83e81436ea3fa5d6c7
Ho Jun Lee <hoojunee@gmail.com>
2024-04-25 20:40:48 +0900

    24.04.25 vue_typescript

---
+++ inHouse/06.Vue.js/vue-ts/src/App.vue
@@ -0,0 +1,30 @@
+<script setup lang="ts">
+let name = "DeAngelo";
+let age = 38;
+let gay = false;
+let occupation: string;
+
+age = 32;
+occupation = "Software Developer";
+
+const arrayOfNumbers: (string | number)[] = [];
+
+interface Employee {
+  name: string;
+  age: number;
+  gay: boolean;
+  occupation?: string;
+}
+
+const employee: Employee = {
+  name: "Brent",
+  age: 29,
+  gay: true,
+};
+
+employee.occupation = "Frontend Developer";
+</script>
+
+<template>
+  <main></main>
+</template>
